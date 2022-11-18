b406ccd07902436ddff7956bb146ab3e283b7a04
Jayhmz <Jayhmz10@gmail.com>
2022-11-18 23:52:34 +0100

    feat(BE-37): update JobSubmissionRepositoryImpl

--- Apply-For-Me-Api/src/main/java/com/hydraulic/applyforme/repository/impl/JobSubmissionRepositoryImpl.java
+++ Apply-For-Me-Api/src/main/java/com/hydraulic/applyforme/repository/impl/JobSubmissionRepositoryImpl.java
@@ -6,27 +6,31 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 
+import org.springframework.stereotype.Repository;
+
 import com.hydraulic.applyforme.model.domain.Submission;
 import com.hydraulic.applyforme.repository.JobSubmissionRepository;
 
+@Repository
 public class JobSubmissionRepositoryImpl implements JobSubmissionRepository {
 
 	@PersistenceContext
 	private EntityManager entityManager;
 
 	@Override
-	public List<Submission> getAll(Long pID, Integer pageOffset) {
+	public List<Submission> getAllSubmissions(Long pID, Integer pageOffset) {
 		
-		final int DEFAULT_PAGE_SIZE = 5;
+		final int DEFAULT_PAGE_SIZE = 10;
 		
 		String getAllSubmission = "SELECT sbm "
 								+ "FROM Submission sbm"
-								+ " WHERE sbm.professional = professional";
+								+ " WHERE sbm.professional = professional"
+								+ " ORDER BY sbm.createdOn DESC";
 		
 		TypedQuery<Submission> allProfessionalSubmissions = entityManager.createQuery(getAllSubmission, Submission.class);
 		allProfessionalSubmissions.setParameter("professional", pID);
 		
-		allProfessionalSubmissions.setFirstResult((pageOffset - 1) * DEFAULT_PAGE_SIZE);
+		allProfessionalSubmissions.setFirstResult(pageOffset - 1);
 		allProfessionalSubmissions.setMaxResults(DEFAULT_PAGE_SIZE);
 		
 		return allProfessionalSubmissions.getResultList();
