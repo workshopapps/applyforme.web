b746d8505e9054db56b30ce058defc9d41b42db7
Jayhmz <Jayhmz10@gmail.com>
2022-11-19 09:22:24 +0100

    feat(BE-37): implement the pagination and sorting of repository interface

--- Apply-For-Me-Api/src/main/java/com/hydraulic/applyforme/repository/impl/JobSubmissionRepositoryImpl.java
+++ Apply-For-Me-Api/src/main/java/com/hydraulic/applyforme/repository/impl/JobSubmissionRepositoryImpl.java
@@ -14,28 +14,40 @@ import com.hydraulic.applyforme.repository.JobSubmissionRepository;
 @Repository
 public class JobSubmissionRepositoryImpl implements JobSubmissionRepository {
 
+	final int DEFAULT_PAGE_SIZE = 10;
+
 	@PersistenceContext
 	private EntityManager entityManager;
 
 	@Override
-	public List<Submission> getAllSubmissions(Long pID, Integer pageOffset) {
-		
-		final int DEFAULT_PAGE_SIZE = 10;
-		
-		String getAllSubmission = "SELECT sbm "
-								+ "FROM Submission sbm"
-								+ " WHERE sbm.professional = professional"
-								+ " ORDER BY sbm.createdOn DESC";
-		
-		TypedQuery<Submission> allProfessionalSubmissions = entityManager.createQuery(getAllSubmission, Submission.class);
+	public List<Submission> getAllSubmissionsByPagination(Long pID, Integer pageOffset) {
+
+		String getAllSubmission = "SELECT sbm " + "FROM Submission sbm" + " WHERE sbm.professional = professional"
+				+ " ORDER BY sbm.createdOn DESC";
+
+		TypedQuery<Submission> allProfessionalSubmissions = entityManager.createQuery(getAllSubmission,
+				Submission.class);
 		allProfessionalSubmissions.setParameter("professional", pID);
-		
+
 		allProfessionalSubmissions.setFirstResult(pageOffset - 1);
 		allProfessionalSubmissions.setMaxResults(DEFAULT_PAGE_SIZE);
-		
+
 		return allProfessionalSubmissions.getResultList();
 	}
 
-	
-	
+	@Override
+	public List<Submission> getallSubmissionsSortedByField(Long pId, Integer pageOffset, String field) {
+		String getAllSubmission = "SELECT sbm " + "FROM Submission sbm" + " WHERE sbm.professional = professional"
+				+ " ORDER BY sbm.jobTitle ASC";
+
+		TypedQuery<Submission> allProfessionalSubmissions = entityManager.createQuery(getAllSubmission,
+				Submission.class);
+		allProfessionalSubmissions.setParameter("professional", pId);
+
+		allProfessionalSubmissions.setFirstResult(pageOffset - 1 );
+		allProfessionalSubmissions.setMaxResults(DEFAULT_PAGE_SIZE);
+
+		return allProfessionalSubmissions.getResultList();
+	}
+
 }
