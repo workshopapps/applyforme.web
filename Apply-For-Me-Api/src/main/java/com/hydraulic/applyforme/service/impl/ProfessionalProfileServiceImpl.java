8d2770a78da3795fe462db85a2739d91f1368b6f
Bradley <39657175+bradl33@users.noreply.github.com>
2022-11-19 17:56:41 +0300

    feat: Added ProfessionalProfileServiceImpl.java

---
+++ Apply-For-Me-Api/src/main/java/com/hydraulic/applyforme/service/impl/ProfessionalProfileServiceImpl.java
@@ -0,0 +1,37 @@
+package com.hydraulic.applyforme.service.impl;
+
+import com.hydraulic.applyforme.model.domain.ProfessionalProfile;
+import com.hydraulic.applyforme.model.dto.professionalProfile.ProfessionalProfileDto;
+import com.hydraulic.applyforme.repository.jpa.ProfessionalProfileJpaRepository;
+import com.hydraulic.applyforme.service.ProfessionalProfileService;
+import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Slf4j
+@Service
+public class ProfessionalProfileServiceImpl implements ProfessionalProfileService {
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    private final ProfessionalProfileJpaRepository repository;
+
+
+    public ProfessionalProfileServiceImpl(ProfessionalProfileJpaRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    @Transactional
+    public ProfessionalProfile createProfile(ProfessionalProfileDto body) {
+
+        ProfessionalProfile professionalProfile;
+        professionalProfile = modelMapper.map(body, ProfessionalProfile.class);
+
+        return repository.save(professionalProfile);
+    }
+}
