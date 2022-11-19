fd1c2b360361dc8fd47c920331b50ef6fd6314b7
Manas Ranjan Patra <53658835+ManasDroid@users.noreply.github.com>
2022-11-20 04:54:02 +0530

    added getAllAppliers endpoint

---
+++ Apply-For-Me-Api/src/main/java/com/hydraulic/applyforme/service/ApplierService.java
@@ -0,0 +1,9 @@
+package com.hydraulic.applyforme.service;
+
+import com.hydraulic.applyforme.model.dto.ApplierDto;
+
+import java.util.List;
+
+public interface ApplierService {
+    List<ApplierDto> getAllAppliers();
+}
