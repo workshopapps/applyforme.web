d8bfdf90344f21da8184a555be5464389b217d68
IZEKOR PAUL <izekorpaul297@gmail.com>
2022-11-19 22:15:29 +0100

    about page

--- Apply-For-Me-UI/src/pages/about_us/AboutUs.jsx
+++ Apply-For-Me-UI/src/pages/about_us/AboutUs.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import Navbar from "./Navbar";
+import Copy from './Copy';
+import Footer from './Footer';
+import Header from './Header';
+import MidContent from './MidContent';
+import What from './What';
+import Who from './Who';
 
 const AboutUs = () => {
-    return <div>AboutUs</div>;
+    return (
+        <>
+            <Navbar />
+            <Header />
+            <Who />
+            <MidContent />
+            <What />
+            <Footer />
+            <Copy />
+        </>
+    )
 };
 
 export default AboutUs;
