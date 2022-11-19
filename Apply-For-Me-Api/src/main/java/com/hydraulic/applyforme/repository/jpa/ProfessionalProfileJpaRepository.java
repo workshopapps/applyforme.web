85689717cca7da20275d3a3b3f30bb1eb59f25ca
Bradley <39657175+bradl33@users.noreply.github.com>
2022-11-19 17:53:55 +0300

    feat: Created ProfessionalProfileJpaRepository.java

---
+++ Apply-For-Me-Api/src/main/java/com/hydraulic/applyforme/repository/jpa/ProfessionalProfileJpaRepository.java
@@ -0,0 +1,8 @@
+package com.hydraulic.applyforme.repository.jpa;
+
+import com.hydraulic.applyforme.model.domain.ProfessionalProfile;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProfessionalProfileJpaRepository extends JpaRepository<ProfessionalProfile, Long> {
+
+}
