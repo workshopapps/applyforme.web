bcb17241af9c32ff8e938f851fa33fa2b1fdf9fb
volunux <volunux@gmail.com>
2022-11-19 19:03:15 +0100

    update controllers

--- Apply-For-Me-Api/src/main/java/com/hydraulic/applyforme/repository/jpa/MemberJaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.hydraulic.applyforme.repository.jpa;
-
-import com.hydraulic.applyforme.model.domain.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberJaRepository extends JpaRepository<Member, Long> {
-}
