12689ea4a97355aae2bd7991290e1b2447f2c61a
anointed osara <anointedosara@gmail.com>
2022-11-19 19:18:40 +0530

    feat: adding my codes

---
+++ Apply-For-Me-UI/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Dashboard from './pages/Dashboard';
+
+function App() {
+  return (
+    <div className="App">
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
