ccff67a3ffe4bbf4c6892dc73e48937c86207ac5
Valentine Ekechukwu <val.ekechukwu@gmail.com>
2022-11-19 08:28:47 +0100

    Update ApplicationSubmissionsRepository.java

--- Apply-For-Me-Api/src/main/java/com/hydraulic/applyforme/repository/ApplicationSubmissionsRepository.java
+++ Apply-For-Me-Api/src/main/java/com/hydraulic/applyforme/repository/ApplicationSubmissionsRepository.java
@@ -4,10 +4,13 @@ import com.hydraulic.applyforme.model.domain.Submission;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.NoRepositoryBean;
 
 import java.util.List;
 
-public interface ApplicationSubmissionsRepository /*extends JpaRepository<Submission, Long>*/ {
+@NoRepositoryBean
+public interface ApplicationSubmissionsRepository extends ApplyForMeRepository {
 
     Page<Submission> getAllSubmissions(List<Submission> submissions, Pageable pageable);
+
 }
