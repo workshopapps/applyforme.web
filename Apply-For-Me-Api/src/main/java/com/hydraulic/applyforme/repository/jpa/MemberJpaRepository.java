b5f7046ae61d293aca8bfe6fb15b55a813f55c25
Busayo Dada <toyinoluwabusayo@gmail.com>
2022-11-20 07:38:19 -0800

    update

--- Apply-For-Me-Api/src/main/java/com/hydraulic/applyforme/repository/jpa/MemberJpaRepository.java
+++ Apply-For-Me-Api/src/main/java/com/hydraulic/applyforme/repository/jpa/MemberJpaRepository.java
@@ -3,6 +3,7 @@ package com.hydraulic.applyforme.repository.jpa;
 import com.hydraulic.applyforme.model.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 import java.util.List;
 
 public interface MemberJpaRepository extends JpaRepository<Member, Long> {
