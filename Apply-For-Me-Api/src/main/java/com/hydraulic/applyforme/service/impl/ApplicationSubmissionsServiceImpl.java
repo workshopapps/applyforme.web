3bda340fa973f91e0b2eca7f23f861fd2939eb87
Valentine Ekechukwu <val.ekechukwu@gmail.com>
2022-11-20 10:57:36 +0100

    Update ApplicationSubmissionsServiceImpl.java

--- Apply-For-Me-Api/src/main/java/com/hydraulic/applyforme/service/impl/ApplicationSubmissionsServiceImpl.java
+++ Apply-For-Me-Api/src/main/java/com/hydraulic/applyforme/service/impl/ApplicationSubmissionsServiceImpl.java
@@ -2,54 +2,65 @@ package com.hydraulic.applyforme.service.impl;
 
 
 import com.hydraulic.applyforme.model.domain.Submission;
+import com.hydraulic.applyforme.repository.ApplicationSubmissionsRepository;
 import com.hydraulic.applyforme.service.ApplicationSubmissionsService;
-import com.hydraulic.applyforme.view.ApplicationSubmissionsResponse;
-import lombok.AllArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
-import java.awt.print.Pageable;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Collection;
 
 @Service
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class ApplicationSubmissionsServiceImpl implements ApplicationSubmissionsService {
 
-    private Submission submission;
-
-    private List<ApplicationSubmissionsResponse> getAllSubmissions(Page<Submission> submissionList){
-
-        List<ApplicationSubmissionsResponse>  applicationSubmissionsResponseList = new ArrayList<>();
-
-        ApplicationSubmissionsResponse applicationSubmissionsResponse =
-                ApplicationSubmissionsResponse.builder()
-                        .applier(submission.getApplier().toString())
-                        .jobCompany(submission.getJobCompany().toString())
-                        .jobLink(submission.getJobLink().toString())
-                        .jobLocation(submission.getJobLocation().toString())
-                        .jobLocationType(submission.getJobLocationType().toString())
-                        .jobTitle(submission.getJobTitle().toString())
-                        .otherComment(submission.getOtherComment().toString())
-                        .summary(submission.getSummary().toString())
-                        .createdOn(submission.getCreatedOn().toString())
-                        .updatedOn(submission.getUpdatedOn().toString())
-                        .build();
-
-        submissionList.forEach(submission -> {
-            applicationSubmissionsResponseList.add(applicationSubmissionsResponse);
-        });
-        return applicationSubmissionsResponseList;
-    }
+    private final ModelMapper modelMapper;
+    //    private Submission submission;
+    //    private final ResponseMapper responseMapper;
+    private final ApplicationSubmissionsRepository applicationSubmissionsRepository;
 
     @Override
-    public List<ApplicationSubmissionsResponse> getAllApplicationSubmissions(int offset, int pageSize) {
-        Pageable pageable = (Pageable) PageRequest.of(offset, pageSize);
+    public Collection<Submission> getAllApplicationSubmissions() {
+        Collection<Submission> submissions = applicationSubmissionsRepository.findAll();
+        return submissions;
 
-//        Page<Submission> submissionsList = applicationSubmissionsDTO.getAllApplicationSubmisions(pageable);
-
-//        return submissionsList (submissionsList);
-        return null;
     }
+
+//    private List<ApplicationSubmissionsResponse> getAllSubmissions(Page<Submission> submissionList){
+//
+//        List<ApplicationSubmissionsResponse>  applicationSubmissionsResponseList = new ArrayList<>();
+//
+//        ApplicationSubmissionsResponse applicationSubmissionsResponse =
+//                ApplicationSubmissionsResponse.builder()
+//                        .applier(submission.getApplier().toString())
+//                        .jobCompany(submission.getJobCompany().toString())
+//                        .jobLink(submission.getJobLink().toString())
+//                        .jobLocation(submission.getJobLocation().toString())
+//                        .jobLocationType(submission.getJobLocationType().toString())
+//                        .jobTitle(submission.getJobTitle().toString())
+//                        .otherComment(submission.getOtherComment().toString())
+//                        .summary(submission.getSummary().toString())
+//                        .createdOn(submission.getCreatedOn().toString())
+//                        .updatedOn(submission.getUpdatedOn().toString())
+//                        .build();
+//
+//        submissionList.forEach(submission -> {
+//            applicationSubmissionsResponseList.add(applicationSubmissionsResponse);
+//        });
+//        return applicationSubmissionsResponseList;
+//    }
+
+//    private List<ApplicationSubmissionsResponse> getAllApplicationSubmissionResponse(Page<Submission> submissionList) {
+//
+//        List<ApplicationSubmissionsResponse> applicationSubmissionsResponseList = new ArrayList<>();
+//
+//        submissionList.forEach(submission -> {
+//            ApplicationSubmissionsResponse response = responseMapper.appSubmissionsResponseToSubmissionMapper(submission);
+//            applicationSubmissionsResponseList.add(response);
+//        });
+//
+//        return applicationSubmissionsResponseList;
+//    }
+
+
 }
