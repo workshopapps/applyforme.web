088ff7a49797cb59c20e7846b12e7dfecc519ae8
Manas Ranjan Patra <53658835+ManasDroid@users.noreply.github.com>
2022-11-20 21:05:32 +0530

    advised updates done

--- Apply-For-Me-Api/src/main/java/com/hydraulic/applyforme/controller/ApplierController.java
+++ Apply-For-Me-Api/src/main/java/com/hydraulic/applyforme/controller/ApplierController.java
@@ -2,20 +2,27 @@ package com.hydraulic.applyforme.controller;
 
 import com.hydraulic.applyforme.model.dto.ApplierDto;
 import com.hydraulic.applyforme.service.ApplierService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@RestController
+@RequestMapping(value = "applier")
 public class ApplierController {
 
-    @Autowired
-    private ApplierService applierService;
 
-    @GetMapping("/api/v1/appliers")
-    public ResponseEntity<List<ApplierDto>> getAllAppliers(){
-        return ResponseEntity.ok(this.applierService.getAllAppliers());
+    private final ApplierService applierService;
+
+    public ApplierController(ApplierService applierService) {
+        this.applierService = applierService;
+    }
+
+
+    @GetMapping("/appliers")
+    public List<ApplierDto> getAllAppliers(){
+        return applierService.getAllAppliers();
     }
 
 }
