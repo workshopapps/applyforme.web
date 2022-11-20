85f385f44668a7697714daa39cc12b14010b8849
hamzat <lawalhamzat27@gmail.com>
2022-11-20 11:53:20 +0100

    Updated

--- Apply-For-Me-UI/src/pages/about_us/AboutUs.jsx
+++ Apply-For-Me-UI/src/pages/about_us/AboutUs.jsx
@@ -1,31 +1,7 @@
 import React from "react";
-<<<<<<< HEAD
 
 const AboutUs = () => {
     return <div>AboutUs</div>;
-=======
-//import Navbar from "./Navbar";
-import Copy from "./Copy";
-import Footer from "./Footer";
-import Header from "./Header";
-import MidContent from "./MidContent";
-import What from "./What";
-import Who from "./Who";
-import Nav from "../../components/nav/Nav";
-
-const AboutUs = () => {
-    return (
-        <>
-            <Nav />
-            <Header />
-            <Who />
-            <MidContent />
-            <What />
-            <Footer />
-            <Copy />
-        </>
-    );
->>>>>>> f47aafc17494e8e145a85e3c9cacc1abe1133f43
 };
 
 export default AboutUs;
