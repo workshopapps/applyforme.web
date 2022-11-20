1a439cd8b92ae69a6773ec92da802a642eaa7cc4
Harny <111377800+Nogist@users.noreply.github.com>
2022-11-20 14:46:02 -0800

    Revert "Feat/fe authentication page"

--- Apply-For-Me-UI/src/App.jsx
+++ Apply-For-Me-UI/src/App.jsx
@@ -24,15 +24,6 @@ import Signs from "./pages/blog/pages/signs/Signs";
 import Resume from "./pages/blog/pages/resume/Resume";
 import Brand from "./pages/blog/pages/brand/Brand";
 import Endorsment from "./pages/blog/pages/endorsement/Endorsement";
-
-import Welcome2 from "./pages/authentication-pages/Welcome2";
-import Registration from "./pages/authentication-pages/Registration";
-import Welcome1 from "./pages/authentication-pages/Welcome1";
-import Verification from "./pages/authentication-pages/Verification";
-import Password from "./pages/authentication-pages/Password";
-import NewPass from "./pages/authentication-pages/NewPass";
-import Button from "./pages/authentication-pages/components/Elements/Button";
-
 import JobDescription from "./pages/job_decription/JobDescription";
 // import AdminDashboard from "./pages/admin_dashboard/AdminDashboard";
 import ApplicantDetails from "./pages/admin_dashboard/components/applicant_details/ApplicantDetails";
@@ -73,16 +64,6 @@ function App() {
                 <Route path="dashboard" element={<UserDashboardLayout />}>
                     <Route path="/dashboard/" element={<DashboardNothing />} />
                 </Route>
-
-
-                <Route exact path='/wel1' element={<Welcome1/>}/>
-                <Route exact path='/reg' element={<Registration/>}/>
-                <Route exact path='/wel2' element={<Welcome2/>}/>
-                <Route exact path='/pass' element={<Password/>}/>
-                <Route exact path='/veri' element={<Verification/>}/>     
-                <Route exact path='/nwpass' element={<NewPass/>}/> 
-                <Route exact path='/nwpass' element={<Button/>}/>   
-
                 <Route
                     path="/dashboard/applications"
                     element={<ApplicationsDashboardLayout />}
