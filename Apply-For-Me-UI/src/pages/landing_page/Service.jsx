b8f12738ec9790a94c5642a6f714c88748483f49
ginohmk <michotall95@gmail.com>
2022-11-22 23:43:16 +0100

    feat: Add landing page

--- Apply-For-Me-UI/src/pages/landing_page/Service.jsx
+++ Apply-For-Me-UI/src/pages/landing_page/Service.jsx
@@ -1,23 +1,35 @@
-import React from 'react'
-import classes from "./Hero.module.css"
-import tick from "../../assets/images/tick.png"
+import React from "react";
+import classes from "./Hero.module.css";
+import tick from "../../assets/images/tick.png";
 
 const Service = () => {
     return (
         <div className={classes.services_container}>
             <div className={classes.services_content}>
-                    <p>We believe career is life, life is once, it is therefore worth enjoying.Study, work, travel, tour, worship, keep working and keep exploring.... It begins with you. We believe career is life, life is once, it is therefore worth enjoying.</p>
+                <div>
+                    <p>
+                        We believe career is life, life is once, it is therefore
+                        worth enjoying.Study, work, travel, tour, worship, keep
+                        working and keep exploring.... It begins with you. We
+                        believe career is life, life is once, it is therefore
+                        worth enjoying.
+                    </p>
                 </div>
 
                 <div className={classes.box}>
                     <img src={tick} alt="" />
                     <h3>All you have to do is attend Job Interviews </h3>
-                    <p>We believe career is life, life is once, it is therefore worth enjoying.Study, work, travel, tour, worship, keep working and keep exploring.... It begins with you. We believe career is life, life is once, it is therefore worth enjoying.</p>
+                    <p>
+                        We believe career is life, life is once, it is therefore
+                        worth enjoying.Study, work, travel, tour, worship, keep
+                        working and keep exploring.... It begins with you. We
+                        believe career is life, life is once, it is therefore
+                        worth enjoying.
+                    </p>
                 </div>
-
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Service
+export default Service;
