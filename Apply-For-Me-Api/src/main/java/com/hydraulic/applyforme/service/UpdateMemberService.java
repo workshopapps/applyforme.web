e4af2385549099aaf8e7e8a72a009c7607440057
Anakhe Ajayi <id3velope@gmail.com>
2022-11-22 05:56:10 +0100

    Update UpdateMemberService.java

--- Apply-For-Me-Api/src/main/java/com/hydraulic/applyforme/service/UpdateMemberService.java
+++ Apply-For-Me-Api/src/main/java/com/hydraulic/applyforme/service/UpdateMemberService.java
@@ -1,8 +1,6 @@
 package com.hydraulic.applyforme.service;
 
-import com.hydraulic.applyforme.model.domain.Country;
 import com.hydraulic.applyforme.model.domain.Member;
-import com.hydraulic.applyforme.model.dto.country.CountryDto;
 import com.hydraulic.applyforme.model.dto.member.UpdateMemberDto;
 import org.springframework.transaction.annotation.Transactional;
 
