c0d82c23c8ff888ae2321ad6f985848fc3e08123
Jayhmz <Jayhmz10@gmail.com>
2022-11-23 23:43:03 +0100

    feat(BE-14): Create update super-admin password repository logic

--- Apply-For-Me-Api/src/main/java/com/hydraulic/applyforme/repository/SuperAdminRepository.java
+++ Apply-For-Me-Api/src/main/java/com/hydraulic/applyforme/repository/SuperAdminRepository.java
@@ -1,7 +1,9 @@
 package com.hydraulic.applyforme.repository;
 
 import com.hydraulic.applyforme.model.domain.Member;
+import com.hydraulic.applyforme.model.dto.UpdatePasswordDTO;
 
 public interface SuperAdminRepository {
     Member  getOneMember(Long id);
+    Member updatePassword(Long id, UpdatePasswordDTO newPassword);
 }
