396aadcd8b79b13b40d9aa642704630109b7a33f
olathegreat <ademolarotimi2000@gmail.com>
2022-11-23 18:08:36 +0100

    complete contact page

--- Apply-For-Me-UI/src/pages/contact_us/ContactUs.jsx
+++ Apply-For-Me-UI/src/pages/contact_us/ContactUs.jsx
@@ -1,7 +1,49 @@
 import React from "react";
+import Form  from "./components/Form";
+import LeftDetails from "./components/LeftDetails";
+import Footer from "../../components/footer/Footer";
+import Nav from "components/nav/Nav";
+import "./components/ContactStyle.css";
+
 
 const ContactUs = () => {
-    return <div>Conatct us</div>;
+    return (
+        <div className="contact">
+          <div className="contact-nav-wrapper">
+            <Nav/>
+            {/* <Nav/> */}
+          </div>
+    
+    
+    
+          <div className="contact-mid">
+            <div className="form">
+              <Form/>
+            </div>
+            <div className="form-details">
+               <LeftDetails/>
+            </div>
+          </div>
+    
+    
+         <div>
+          <Footer/>
+         </div>
+    
+         {/* <div className="copyright">
+            <p>2022 ApplyForMe. All rights reserved</p>
+    
+            <p>Cookie Policy</p>
+            <p>Terms of Service</p>
+            <p>Cookie Setting</p>
+    
+         </div> */}
+    
+    
+    
+          
+        </div>
+      )
 };
 
 export default ContactUs;
