7bca9cee7948d0b2fd1f00e4c07703e0391fe182
Jayhmz <Jayhmz10@gmail.com>
2022-11-24 10:14:21 +0100

    feat(BE-14): Update SuperAdminRepository

--- Apply-For-Me-Api/src/main/java/com/hydraulic/applyforme/repository/SuperAdminRepository.java
+++ Apply-For-Me-Api/src/main/java/com/hydraulic/applyforme/repository/SuperAdminRepository.java
@@ -1,9 +1,8 @@
 package com.hydraulic.applyforme.repository;
 
 import com.hydraulic.applyforme.model.domain.Member;
-import com.hydraulic.applyforme.model.dto.UpdatePasswordDTO;
 
 public interface SuperAdminRepository {
     Member  getOneMember(Long id);
-    Member updatePassword(Long id, UpdatePasswordDTO newPassword);
+    Member updatePassword(Long id, String newPassword);
 }
