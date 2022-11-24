9ff467498fa22ae6d75b0390b4253c4c6422c1cc
Harny <111377800+Nogist@users.noreply.github.com>
2022-11-24 16:34:23 +0100

    Revert "complete contact page"

--- Apply-For-Me-UI/src/pages/contact_us/ContactUs.jsx
+++ Apply-For-Me-UI/src/pages/contact_us/ContactUs.jsx
@@ -1,46 +1,7 @@
 import React from "react";
-import Form  from "./components/Form";
-import LeftDetails from "./components/LeftDetails";
-import Footer from "../../components/footer/Footer";
-import Nav from "components/nav/Nav";
-import "./components/ContactStyle.css";
-
 
 const ContactUs = () => {
-    return (
-        <div className="contact">
-          <div className="contact-nav-wrapper">
-            <Nav/>
-            {/* <Nav/> */}
-          </div>
-    
-    
-    
-            <div className="form">
-            <div className="form-details">
-               <LeftDetails/>
-            </div>
-          </div>
-    
-    
-         <div>
-          <Footer/>
-         </div>
-    
-         {/* <div className="copyright">
-            <p>2022 ApplyForMe. All rights reserved</p>
-    
-            <p>Cookie Policy</p>
-            <p>Terms of Service</p>
-            <p>Cookie Setting</p>
-    
-         </div> */}
-    
-    
-    
-          
-        </div>
-      )
+    return <div>Conatct us</div>;
 };
 
 export default ContactUs;
