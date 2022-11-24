28cbcf8eb95e5534812f7e0a1f17714e13cb57c7
Jayhmz <Jayhmz10@gmail.com>
2022-11-24 09:10:49 +0100

    feat(BE-14): create and implement update password repository logic

--- Apply-For-Me-Api/src/main/java/com/hydraulic/applyforme/repository/impl/SuperAdminRepositoryImpl.java
+++ Apply-For-Me-Api/src/main/java/com/hydraulic/applyforme/repository/impl/SuperAdminRepositoryImpl.java
@@ -1,11 +1,13 @@
 package com.hydraulic.applyforme.repository.impl;
 
 import com.hydraulic.applyforme.model.domain.Member;
+import com.hydraulic.applyforme.model.dto.UpdatePasswordDTO;
 import com.hydraulic.applyforme.repository.SuperAdminRepository;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 
 @Repository
 public class SuperAdminRepositoryImpl implements SuperAdminRepository {
@@ -18,4 +20,16 @@ public class SuperAdminRepositoryImpl implements SuperAdminRepository {
     public Member getOneMember(Long id) {
         return entityManager.find(Member.class, id);
     }
+
+	@Override
+	public Member updatePassword(Long id, UpdatePasswordDTO password) {
+		
+		String query = "UPDATE Member m SET m.password = :newPassword WHERE m.id = :memberId";
+		TypedQuery<Member> updatedMember = entityManager.createQuery(query, Member.class);
+		updatedMember.setParameter("newPassword", password.getNewPassword());
+		updatedMember.setParameter("memberId", id);
+		
+		return updatedMember.getSingleResult();
+	}
+    
 }
--- Apply-For-Me-Api/src/main/java/com/hydraulic/applyforme/service/impl/SuperAdminServiceImpl.java
+++ Apply-For-Me-Api/src/main/java/com/hydraulic/applyforme/service/impl/SuperAdminServiceImpl.java
@@ -1,9 +1,13 @@
 package com.hydraulic.applyforme.service.impl;
 
 import com.hydraulic.applyforme.model.domain.Member;
+import com.hydraulic.applyforme.model.dto.UpdatePasswordDTO;
 import com.hydraulic.applyforme.model.exception.MemberNotFoundException;
 import com.hydraulic.applyforme.repository.SuperAdminRepository;
 import com.hydraulic.applyforme.service.SuperAdminService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -11,6 +15,9 @@ import java.util.Optional;
 @Service
 public class SuperAdminServiceImpl implements SuperAdminService {
 
+	@Autowired
+	private PasswordEncoder encoder;
+	
     SuperAdminRepository repository;
 
     public SuperAdminServiceImpl(SuperAdminRepository repository) {
@@ -24,5 +31,23 @@ public class SuperAdminServiceImpl implements SuperAdminService {
         }
        return member;
     }
+
+	@Override
+	public void updatePasswordById(Long id, UpdatePasswordDTO updatePasswordDTO) {
+		
+		//get the member
+		Member member = getDetailsById(id);
+    	//check the initial password string against the saved encoded password 
+		boolean matches = encoder.matches(updatePasswordDTO.getInitialPassword(), member.getPassword());
+		if(matches) {
+			//update the password
+
+			member.setPassword(updatePasswordDTO.getNewPassword());
+		}
+    	
+    	
+    	//return the member
+		
+	}
 
 }
