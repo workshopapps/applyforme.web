26f1bc5acf99d9e714c47119faa0f353ae114321
Jayhmz <Jayhmz10@gmail.com>
2022-11-24 10:14:38 +0100

    feat(BE-14): Update SuperAdminRepositoryImpl

--- Apply-For-Me-Api/src/main/java/com/hydraulic/applyforme/repository/impl/SuperAdminRepositoryImpl.java
+++ Apply-For-Me-Api/src/main/java/com/hydraulic/applyforme/repository/impl/SuperAdminRepositoryImpl.java
@@ -1,13 +1,13 @@
 package com.hydraulic.applyforme.repository.impl;
 
-import com.hydraulic.applyforme.model.domain.Member;
-import com.hydraulic.applyforme.model.dto.UpdatePasswordDTO;
-import com.hydraulic.applyforme.repository.SuperAdminRepository;
-import org.springframework.stereotype.Repository;
-
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
+
+import org.springframework.stereotype.Repository;
+
+import com.hydraulic.applyforme.model.domain.Member;
+import com.hydraulic.applyforme.repository.SuperAdminRepository;
 
 @Repository
 public class SuperAdminRepositoryImpl implements SuperAdminRepository {
@@ -22,11 +22,11 @@ public class SuperAdminRepositoryImpl implements SuperAdminRepository {
     }
 
 	@Override
-	public Member updatePassword(Long id, UpdatePasswordDTO password) {
+	public Member updatePassword(Long id, String newPassword) {
 		
 		String query = "UPDATE Member m SET m.password = :newPassword WHERE m.id = :memberId";
 		TypedQuery<Member> updatedMember = entityManager.createQuery(query, Member.class);
-		updatedMember.setParameter("newPassword", password.getNewPassword());
+		updatedMember.setParameter("newPassword", newPassword);
 		updatedMember.setParameter("memberId", id);
 		
 		return updatedMember.getSingleResult();
