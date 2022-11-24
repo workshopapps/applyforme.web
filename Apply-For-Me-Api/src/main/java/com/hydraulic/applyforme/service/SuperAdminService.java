5295492d3373a2f9588a7cdb71aac8f4d1ad7580
Jayhmz <Jayhmz10@gmail.com>
2022-11-24 09:12:04 +0100

    feat(BE-14): create update password logic for SuperAdminService

--- Apply-For-Me-Api/src/main/java/com/hydraulic/applyforme/service/SuperAdminService.java
+++ Apply-For-Me-Api/src/main/java/com/hydraulic/applyforme/service/SuperAdminService.java
@@ -1,8 +1,11 @@
 package com.hydraulic.applyforme.service;
 
 import com.hydraulic.applyforme.model.domain.Member;
+import com.hydraulic.applyforme.model.dto.UpdatePasswordDTO;
 
 public interface SuperAdminService {
 
     Member getDetailsById(Long id);
+
+	public void updatePasswordById(Long id, UpdatePasswordDTO updatePasswordDTO);
 }
