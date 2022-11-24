95b7dba5dc7fb82b2f6520669ca4fbd444a17510
IZEKOR PAUL <izekorpaul297@gmail.com>
2022-11-24 17:03:08 +0100

    making pull request to dev branch

--- Apply-For-Me-UI/src/pages/about_us/Header.js
+++ Apply-For-Me-UI/src/pages/about_us/Header.js
@@ -5,7 +5,8 @@ function Header() {
         <div className="header">
             <div className="blur">
             <h1 className="header-text">
-            Our Mission is Simple: To help you search, apply and secure jobs that help you advance your career without stress
+            Our Mission is Simple: To help you search, 
+            apply and secure jobs that help you advance your career without stress
             </h1>
             </div>
         </div>
