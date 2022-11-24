435e5298fe431b632fe06aaf843cc2dc1476340b
Jayhmz <Jayhmz10@gmail.com>
2022-11-24 10:18:39 +0100

    feat(BE-14): Update SuperAdminServiceIMpl

--- Apply-For-Me-Api/src/main/java/com/hydraulic/applyforme/service/impl/SuperAdminServiceImpl.java
+++ Apply-For-Me-Api/src/main/java/com/hydraulic/applyforme/service/impl/SuperAdminServiceImpl.java
@@ -1,16 +1,15 @@
 package com.hydraulic.applyforme.service.impl;
 
-import com.hydraulic.applyforme.model.domain.Member;
-import com.hydraulic.applyforme.model.dto.UpdatePasswordDTO;
-import com.hydraulic.applyforme.model.exception.MemberNotFoundException;
-import com.hydraulic.applyforme.repository.SuperAdminRepository;
-import com.hydraulic.applyforme.service.SuperAdminService;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
+import com.hydraulic.applyforme.model.domain.Member;
+import com.hydraulic.applyforme.model.dto.UpdatePasswordDTO;
+import com.hydraulic.applyforme.model.exception.MemberNotFoundException;
+import com.hydraulic.applyforme.model.exception.PasswordMismatchException;
+import com.hydraulic.applyforme.repository.SuperAdminRepository;
+import com.hydraulic.applyforme.service.SuperAdminService;
 
 @Service
 public class SuperAdminServiceImpl implements SuperAdminService {
@@ -33,21 +32,17 @@ public class SuperAdminServiceImpl implements SuperAdminService {
     }
 
 	@Override
-	public void updatePasswordById(Long id, UpdatePasswordDTO updatePasswordDTO) {
+	public void updatePasswordById(Long id, UpdatePasswordDTO updatePasswordDTO) throws PasswordMismatchException {
 		
-		//get the member
 		Member member = getDetailsById(id);
-    	//check the initial password string against the saved encoded password 
-		boolean matches = encoder.matches(updatePasswordDTO.getInitialPassword(), member.getPassword());
-		if(matches) {
-			//update the password
-
-			member.setPassword(updatePasswordDTO.getNewPassword());
-		}
-    	
-    	
-    	//return the member
 		
+		boolean matches = encoder.matches(updatePasswordDTO.getInitialPassword(), member.getPassword());
+		
+		if(!matches) {
+			throw new PasswordMismatchException();
+		}
+		
+		repository.updatePassword(id, updatePasswordDTO.getNewPassword());		
 	}
 
 }
