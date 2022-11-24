a21ccd3b05328a8ad1d5850feead1bfd1f2c4e56
#Lay <82811450+AleeyahAnjola@users.noreply.github.com>
2022-11-24 14:59:06 +0100

    testing the code

--- Apply-For-Me-Api/src/main/java/com/hydraulic/applyforme/repository/SuperAdminRepository.java
+++ Apply-For-Me-Api/src/main/java/com/hydraulic/applyforme/repository/SuperAdminRepository.java
@@ -5,4 +5,14 @@ import com.hydraulic.applyforme.model.domain.Member;
 public interface SuperAdminRepository {
     Member  getOneMember(Long id);
     Boolean removeMemberById(Long id);
+
+/*
+ * REPOSITORY JPA REPO
+ * @Query("select m from Member m )
+ *  Member findAll();
+ *  List<Member> getAll();
+ *
+ * SERVICE INTERFACE:
+ * List<Member> viewRecruiters();
+ */
 }
