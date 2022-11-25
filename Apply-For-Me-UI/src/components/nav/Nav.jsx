96fd658fc20f10aa3d7ae01637f99083fc64d8f2
ginohmk <michotall95@gmail.com>
2022-11-25 14:13:27 +0100

    Add nav mobile dropdown

--- Apply-For-Me-UI/src/components/nav/Nav.jsx
+++ Apply-For-Me-UI/src/components/nav/Nav.jsx
@@ -1,15 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { FiMenu } from "react-icons/fi";
+import { FiMenu, FiX } from "react-icons/fi";
 import classes from "./Nav.module.css";
 import Logo from "../../assets/images/nav_logo.svg";
 import BlueButton from "../buttons/blue_background/BlueButton";
 import LightButton from "../buttons/light_button/LightButton";
 
 const Nav = () => {
+    const [dropDown, setDropDown] = useState(false);
     return (
         <section className={classes.nav_container}>
-            <nav className={classes.nav_container__wrapper}>
+            <nav
+                className={[
+                    classes.nav_container__wrapper,
+                    classes.hide_mobile
+                ].join(" ")}
+            >
                 <div className={classes.logo}>
                     <Link to="/">
                         <img
@@ -44,9 +50,78 @@ const Nav = () => {
                     <LightButton text="Sign in" width="127" />
                     <BlueButton text="Get started" width="156" />
                 </div>
-
-                <FiMenu className={classes.menu} />
             </nav>
+
+            {/*
+            Mobile
+            */}
+            <div className="hide__desktop">
+                <nav
+                    className={classes.nav_container__wrapper__mobile}
+                    style={{ height: dropDown ? "100vh" : "" }}
+                >
+                    <div className={classes.logo__mobile}>
+                        <Link to="/">
+                            <img
+                                src={Logo}
+                                alt="Apply for me Logo"
+                                className={classes.logo__img__mobile}
+                            />
+                        </Link>
+
+                        <FiMenu
+                            className={classes.menu__mobile}
+                            onClick={() => setDropDown(true)}
+                            style={{ display: dropDown ? "none" : "block" }}
+                        />
+
+                        <FiX
+                            className={classes.menu__mobile}
+                            onClick={() => setDropDown(false)}
+                            style={{ display: dropDown ? "block" : "none" }}
+                        />
+                    </div>
+
+                    <ul
+                        className={classes.nav_links__mobile}
+                        style={{ display: dropDown ? "flex" : "none" }}
+                    >
+                        <li onClick={() => setDropDown(false)}>
+                            <Link to="/about">About us</Link>
+                        </li>
+
+                        <li onClick={() => setDropDown(false)}>
+                            <Link to="/pricing">Pricing plan</Link>
+                        </li>
+
+                        <li onClick={() => setDropDown(false)}>
+                            <Link to="/blog">Blog</Link>
+                        </li>
+                        <li>
+                            <Link to="/faqs">FAQs</Link>
+                        </li>
+                        <li onClick={() => setDropDown(false)}>
+                            <Link to="/contact">Contact us</Link>
+                        </li>
+                    </ul>
+
+                    <div
+                        className={classes.btn_container__mobile}
+                        style={{ display: dropDown ? "flex" : "none" }}
+                    >
+                        <LightButton
+                            text="Sign in"
+                            width="127"
+                            func={() => setDropDown(false)}
+                        />
+                        <BlueButton
+                            text="Get started"
+                            width="156"
+                            func={() => setDropDown(false)}
+                        />
+                    </div>
+                </nav>
+            </div>
         </section>
     );
 };
