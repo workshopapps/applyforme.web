fe91c41561a038274c6dcf84bf710dfd5622e639
Jayhmz <Jayhmz10@gmail.com>
2022-11-25 08:09:47 +0100

    feat(BE-14): Recreate SuperAdminController
    Unknowingly, this controller got removed after the last merge conflict got resolved.

---
+++ Apply-For-Me-Api/src/main/java/com/hydraulic/applyforme/controller/SuperAdminController.java
@@ -0,0 +1,39 @@
+package com.hydraulic.applyforme.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hydraulic.applyforme.model.domain.Member;
+import com.hydraulic.applyforme.model.dto.UpdatePasswordDTO;
+import com.hydraulic.applyforme.service.SuperAdminService;
+
+@RestController
+@RequestMapping(
+        value = "member",
+        produces = { MediaType.APPLICATION_JSON_VALUE }
+)
+public class SuperAdminController {
+
+    SuperAdminService service;
+
+    public SuperAdminController(SuperAdminService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/detail/{id}")
+    public Member getOneMember(@PathVariable(value="id") Long id) {
+        return service.getDetailsById(id);
+    }
+    
+    @PostMapping("/{id}/change-password")
+    public ResponseEntity<?> changePassword(@PathVariable("id") Long id, UpdatePasswordDTO passwordDTO) {
+    	service.updatePasswordById(id, passwordDTO);   	
+
+    	return ResponseEntity.accepted().body("successful");
+    }
+}
