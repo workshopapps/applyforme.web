5d6c8456a55bb2932baa61b230f0f71a0f060c2a
Efezino Idisi <enduranceidisi13@gmail.com>
2022-11-29 18:30:25 +0100

    feat: add form validation to contact page

--- Apply-For-Me-UI/src/pages/contact_us/ContactUs.jsx
+++ Apply-For-Me-UI/src/pages/contact_us/ContactUs.jsx
@@ -5,31 +5,23 @@ import "./components/ContactStyle.css";
 import Nav from "../../components/nav/Nav";
 import Footer from "../../components/footer/Footer";
 
-
 const ContactUs = () => {
     return (
         <div className="contact">
-          
-            <Nav/>
-        
-    
-    
-    
-          <div className="contact-mid">
-            <div className="form">
-              <ContactUsForm/>
+            <Nav />
+
+            <div className="contact-mid">
+                <div className="form">
+                    <ContactUsForm />
+                </div>
+                <div className="form-details">
+                    <LeftDetails />
+                </div>
             </div>
-            <div className="form-details">
-               <LeftDetails/>
-            </div>
-          </div>
-    
-    
-         
-          <Footer/>
-         
-    
-         {/* <div className="copyright">
+
+            <Footer />
+
+            {/* <div className="copyright">
             <p>2022 ApplyForMe. All rights reserved</p>
     
             <p>Cookie Policy</p>
@@ -37,12 +29,8 @@ const ContactUs = () => {
             <p>Cookie Setting</p>
     
          </div> */}
-    
-    
-    
-          
         </div>
-      )
+    );
 };
 
 export default ContactUs;
--- Apply-For-Me-UI/src/pages/contact_us/components/ContactUsForm.js
+++ Apply-For-Me-UI/src/pages/contact_us/components/ContactUsForm.js
@@ -1,75 +1,202 @@
-import React from 'react';
+import React from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
+const onSubmit = (values, actions) => {
+    console.log(values, actions);
+};
 
 const ContactUsForm = () => {
+    const { values, handleBlur, handleChange, touched, errors, handleSubmit } =
+        useFormik({
+            // form state
+            initialValues: {
+                firstName: "",
+                lastName: "",
+                email: "",
+                message: "",
+                check: false,
+                phoneNumber: ""
+            },
 
-    const formSubmit = (e) =>{
-        e.preventDefault();
-        alert("submitted");
-    }
+            //   form validation
+            validationSchema: Yup.object().shape({
+                firstName: Yup.string()
+                    .max(20, "Name must be 20 characters or less.")
+                    .required("Please enter your first name"),
+                lastName: Yup.string()
+                    .max(20, "Name must be 20 characters or less.")
+                    .required("please enter your last name"),
+                email: Yup.string()
+                    .email("Invalid email address")
+                    .required("Email is required"),
+                message: Yup.string().required("please enter your message"),
+                phoneNumber: Yup.number()
+                    .min(11)
+                    .required("Please enter a phone number"),
+                check: Yup.boolean().oneOf(
+                    [true],
+                    "You must agree to the terms"
+                )
+            }),
 
-  return (
-    <div className="form-body">
-        <form onSubmit={formSubmit}>
+            onSubmit
+        });
 
-        
-
-
+    return (
+        <div className="form-body">
+            <form onSubmit={handleSubmit}>
                 <div className="form-first-line">
                     <div className="form-group first-line-1">
-                        <label>First Name</label>
-                        <input type="text" required></input>
-
+                        <label htmlFor="first_name">First Name</label>
+                        <input
+                            type="text"
+                            name="firstName"
+                            id="first_name"
+                            className={
+                                touched.firstName && errors.firstName
+                                    ? "input-error"
+                                    : ""
+                            }
+                            value={values.firstName}
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                        />
+                        {touched.firstName && errors.firstName && (
+                            <small style={{ color: "red" }}>
+                                {errors.firstName}
+                            </small>
+                        )}
                     </div>
 
                     <div className="form-group first-line-2">
-                        <label>Last Name</label>
-                        <input type="text" required></input>
-
+                        <label htmlFor="last_name">Last Name</label>
+                        <input
+                            type="text"
+                            id="last_name"
+                            name="lastName"
+                            className={
+                                touched.lastName && errors.lastName
+                                    ? "input-error"
+                                    : ""
+                            }
+                            placeholder="Enter your last name"
+                            value={values.lastName}
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                        />
+                        {touched.lastName && errors.lastName && (
+                            <small style={{ color: "red" }}>
+                                {errors.lastName}
+                            </small>
+                        )}
                     </div>
                 </div>
 
                 <div className="form-second-line">
-                    <div className='form-group'>
+                    <div className="form-group">
                         <label>Email</label>
-                        <input type="email" required></input>
-                    </div>
+                        <input
+                            type="email"
+                            id="email"
+                            name="email"
+                            className={
+                                touched.email && errors.email
+                                    ? "input-error"
+                                    : ""
+                            }
+                            placeholder="yourname@gmail.com"
+                            value={values.email}
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                        />
 
+                        {touched.email && errors.email && (
+                            <small style={{ color: "red" }}>
+                                {errors.email}
+                            </small>
+                        )}
+                    </div>
                 </div>
 
                 <div className="form-third-line">
                     <div className="form-group">
                         <label>Phone Number</label>
-                        <input type="text" required></input>
+                        <input
+                            type="tel"
+                            id="phoneNumber"
+                            name="phoneNumber"
+                            className={
+                                touched.phoneNumber && errors.phoneNumber
+                                    ? "input-error"
+                                    : ""
+                            }
+                            placeholder="Enter your phone number"
+                            value={values.phoneNumber}
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                        />
+                        {touched.phoneNumber && errors.phoneNumber && (
+                            <small style={{ color: "red" }}>
+                                {errors.phoneNumber}
+                            </small>
+                        )}
                     </div>
                 </div>
 
                 <div className="form-fourth-line">
                     <div className="form-group">
                         <label>Message</label>
-                        <div className="textarea">
-                           <textarea required></textarea>
+                        <div>
+                            <textarea
+                                id="message"
+                                name="message"
+                                cols="30"
+                                rows="5"
+                                className={
+                                    touched.message && errors.message
+                                        ? "textarea-error"
+                                        : "textarea"
+                                }
+                                placeholder="message"
+                                value={values.message}
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                            ></textarea>
+                            {touched.message && errors.message && (
+                                <small style={{ color: "red" }}>
+                                    {errors.message}
+                                </small>
+                            )}
                         </div>
-                       
-
                     </div>
-
                 </div>
 
                 <div className="form-fifth-line">
                     <div className="radio-group">
-                        <input type="radio" required></input>
-                        <label className="privacy"> I have read the afm privacy</label>
+                        <input
+                            type="checkbox"
+                            id="check"
+                            name="check"
+                            value="checked"
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                        />
+
+                        <label className="privacy">
+                            {" "}
+                            I have read the afm privacy
+                        </label>
                     </div>
+                    {touched.check && errors.check && (
+                        <small style={{ color: "red" }}>{errors.check}</small>
+                    )}
                 </div>
 
-                <button>Send Message</button>
-
-        </form>
-      
-    </div>
-  );
-}
+                <button type="submit">Send Message</button>
+            </form>
+        </div>
+    );
+};
 
 export default ContactUsForm;
-
