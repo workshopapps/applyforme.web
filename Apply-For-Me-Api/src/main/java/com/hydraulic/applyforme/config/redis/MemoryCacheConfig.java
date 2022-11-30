396659ab67a087e384dcff6081fec141fc46842a
volunux <volunux@gmail.com>
2022-11-30 10:36:49 +0100

    redis store

--- Apply-For-Me-Api/src/main/java/com/hydraulic/applyforme/config/redis/MemoryCacheConfig.java
+++ Apply-For-Me-Api/src/main/java/com/hydraulic/applyforme/config/redis/MemoryCacheConfig.java
@@ -5,11 +5,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisPassword;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
 
 @Configuration
 public class MemoryCacheConfig {
@@ -28,12 +34,44 @@ public class MemoryCacheConfig {
     }
 
     @Bean
+    public RedisCacheConfiguration cacheConfiguration() {
+        RedisCacheConfiguration cacheConfiguration = RedisCacheConfiguration.defaultCacheConfig();
+        cacheConfiguration
+                .entryTtl(Duration.ofSeconds(600))
+                .disableCachingNullValues();
+        return cacheConfiguration;
+    }
+
+    @Bean
+    public StringRedisSerializer stringRedisSerializer() {
+        return new StringRedisSerializer();
+    }
+
+    @Bean
+    public GenericJackson2JsonRedisSerializer jackson2JsonRedisSerializer() {
+        return new GenericJackson2JsonRedisSerializer();
+    }
+
+    @Bean
     @Primary
-    public RedisTemplate redisTemplate(JedisConnectionFactory connectionFactory) {
+    public RedisTemplate<String, Object> redisTemplate(JedisConnectionFactory connectionFactory) {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
+        template.setKeySerializer(stringRedisSerializer());
+        template.setValueSerializer(jackson2JsonRedisSerializer());
         return template;
     }
+
+    @Bean
+    public RedisCacheManager cacheManager() {
+        RedisCacheManager cacheManager = RedisCacheManager
+                .builder(jedisConnectionFactory())
+                .cacheDefaults(cacheConfiguration())
+                .transactionAware()
+                .build();
+        return cacheManager;
+    }
+
 
     @Bean
     public ValueOperations<String, Object> valueOperations(RedisTemplate redisTemplate) {
--- Apply-For-Me-Api/src/main/java/com/hydraulic/applyforme/service/InMemoryCacheService.java
+++ Apply-For-Me-Api/src/main/java/com/hydraulic/applyforme/service/InMemoryCacheService.java
@@ -4,6 +4,8 @@ public interface InMemoryCacheService {
 
     public void set(String key, Object value);
 
+    void set(String key, Object value, long seconds);
+
     Object get(String key);
 
     boolean exists(String key);
