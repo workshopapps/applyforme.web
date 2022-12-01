c9fada3e5bf17f8af4e8dea96f58180068375684
olathegreat <ademolarotimi2000@gmail.com>
2022-12-01 16:59:34 +0100

    feat:overview card for dashboard design

--- Apply-For-Me-UI/src/pages/dashboard_noting/DashboardNothing.js
+++ Apply-For-Me-UI/src/pages/dashboard_noting/DashboardNothing.js
@@ -1,4 +1,6 @@
 import React from "react";
+import avatar from "./img/avatar.png";
+import notification from "./img/notification.png";
 import "./DashboardNothing.css";
 
 
@@ -21,6 +23,80 @@ const DashboardNothing = () => {
                      </p>
 
                   </div>
+
+
+                  <div className="top-dashboard-right">
+                    <div className="dashboard-img-wrapper">
+                       <img src={notification} alt="icon"></img>
+                    </div>
+
+                    <div className="dashboard-img-wrapper">
+                          <img src={avatar} alt="notification"></img>
+                    </div>
+                   
+                    
+
+                  </div>
+             </section>
+
+             <hr></hr>
+
+
+
+             {/* overview cards */}
+
+
+             <section className="dashboard-overview">
+                <h5>overview</h5>
+
+                <div className="overview-cards-wrapper">
+                    <div className="overview-card-wrapper">
+
+
+                      <div className="overview-card">
+
+                            <h3>72</h3>
+                            <p>Total Appliaction</p>
+
+                        </div>
+
+
+
+                    </div>
+
+                    <div className="overview-card-wrapper">
+                            
+
+                               <div className="overview-card">
+                                    <h3>72</h3>
+                                    <p>Total Appliaction</p>
+                                </div>
+
+
+
+                    </div>
+
+                    <div className="overview-card-wrapper">
+                            
+                            <div className="overview-card">
+                                 <h3>72</h3>
+                                 <p>Total Appliaction</p>
+                             </div>
+
+                   </div>
+
+                  
+                </div>
+
+             </section>
+
+
+             {/* section for my application */}
+
+             
+             <section className="dashboard-myapplication">
+
+
              </section>
         </div>
 
