7b27c1dbac8bcc04d37de39e5ed4b3f08cf2a3c8
olathegreat <ademolarotimi2000@gmail.com>
2022-12-01 16:01:29 +0100

    feat:dashboard page design

--- Apply-For-Me-UI/src/pages/dashboard_noting/DashboardNothing.js
+++ Apply-For-Me-UI/src/pages/dashboard_noting/DashboardNothing.js
@@ -1,7 +1,31 @@
 import React from "react";
+import "./DashboardNothing.css";
+
+
 
 const DashboardNothing = () => {
-    return <div>Nothinhg to see here</div>;
+    return(
+
+        <div className="dashboardnothing">
+
+             {/* this is the top stripe */}
+
+             <section className="top-dashboard-stripe">
+                  <div className="top-dashboard-left">
+                     <h2>
+                        Good Evening Bukola
+
+                     </h2>
+                     <p>
+                       Here's an overview
+                     </p>
+
+                  </div>
+             </section>
+        </div>
+
+
+    )
 };
 
 export default DashboardNothing;
