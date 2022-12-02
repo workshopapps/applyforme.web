397fbfdf30ca655d7fc175f6a852e3e506637996
IZEKOR PAUL <izekorpaul297@gmail.com>
2022-12-02 19:06:02 +0100

    fixed route issues

--- Apply-For-Me-UI/src/pages/applications/layouts/ApplicationsDashboardLayout.jsx
+++ Apply-For-Me-UI/src/pages/applications/layouts/ApplicationsDashboardLayout.jsx
@@ -2,11 +2,13 @@ import { Outlet } from "react-router-dom";
 import Sidebar from "../components/Sidebar";
 import styles from "./ApplicationDashboard.module.css";
 import BottomNav from "../components/BottomNav";
+import DashboardSidebar from "components/dashboard_sidebar/DashboardSidebar";
 
 const ApplicationsDashboardLayout = () => {
     return (
         <main className={styles.layout_container}>
-            <Sidebar />
+            {/* <Sidebar /> */}
+            <DashboardSidebar />
             <div className={styles.layout_main}>
                 <Outlet />
             </div>
