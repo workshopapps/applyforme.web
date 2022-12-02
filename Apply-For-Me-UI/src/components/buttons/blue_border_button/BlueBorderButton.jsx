1ec3877538677ba86a8ac984b1f82900bb8caf44
anointed osara <anointedosara@gmail.com>
2022-12-02 19:20:11 +0530

    fix: Making some changes to the superAdminProfile Page and Adding functionalityto the Pricing page contact us button

--- Apply-For-Me-UI/src/components/buttons/blue_border_button/BlueBorderButton.jsx
+++ Apply-For-Me-UI/src/components/buttons/blue_border_button/BlueBorderButton.jsx
@@ -6,7 +6,6 @@ import { useNavigate } from "react-router-dom";
 const BlueBorderButton = ({ width, text, func }) => {
     const navigate = useNavigate();
     return (
-<<<<<<< HEAD
             <button
                 style={{ width: `${width}px` }}
                 className={[classes.btn, mainStyle.main_btn].join(" ")}
@@ -14,15 +13,6 @@ const BlueBorderButton = ({ width, text, func }) => {
             >
                 {text}
             </button>
-=======
-        <button
-            style={{ width: `${width}px` }}
-            className={[classes.btn, mainStyle.main_btn].join(" ")}
-            onClick={() => (func ? func() : {})}
-        >
-            {text}
-        </button>
->>>>>>> ac402ad696b4a75c65d9a475a9a2c9c771f3163d
     );
 };
 
