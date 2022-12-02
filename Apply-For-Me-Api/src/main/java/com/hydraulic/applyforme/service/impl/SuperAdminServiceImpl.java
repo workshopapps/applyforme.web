2135de0fc1a0854eb09ee9cc1a1fe49ffb2ef2e6
Jayhmz <Jayhmz10@gmail.com>
2022-12-02 17:26:15 +0100

    fix(BE-94) fix merge conflict

--- Apply-For-Me-Api/src/main/java/com/hydraulic/applyforme/service/impl/SuperAdminServiceImpl.java
+++ Apply-For-Me-Api/src/main/java/com/hydraulic/applyforme/service/impl/SuperAdminServiceImpl.java
@@ -5,6 +5,8 @@ import com.hydraulic.applyforme.model.dto.admin.UpdatePasswordDto;
 import com.hydraulic.applyforme.model.exception.MemberNotFoundException;
 import com.hydraulic.applyforme.model.exception.PasswordMismatchException;
 import com.hydraulic.applyforme.repository.MemberRepository;
+import com.hydraulic.applyforme.repository.jpa.MemberJpaRepository;
+import com.hydraulic.applyforme.repository.jpa.RoleJpaRepository;
 import com.hydraulic.applyforme.repository.jpa.SuperAdminJpaRepository;
 import com.hydraulic.applyforme.service.SuperAdminService;
 import org.modelmapper.ModelMapper;
@@ -21,6 +23,11 @@ public class SuperAdminServiceImpl implements SuperAdminService {
     private ModelMapper mapper;
     @Autowired
     private PasswordEncoder passwordEncoder;
+    
+    @Autowired
+    private MemberJpaRepository memberJpaReposiroty;
+    @Autowired
+    private RoleJpaRepository roleJpaRepository;
     
     @Autowired
     private SuperAdminJpaRepository jpaRepository;
@@ -58,13 +65,20 @@ public class SuperAdminServiceImpl implements SuperAdminService {
     @Override
     @Transactional
     public boolean deleteMemberById(Long id) {
-        boolean removed = repository.remove(id);
-        if(removed){
-            return true;
-        }
-        else {
-            throw new MemberNotFoundException(id);
-        }
+    	Member member = memberJpaReposiroty.getById(id);
+    	if(member == null) {
+    		throw new MemberNotFoundException(id);
+    	}
+    	memberJpaReposiroty.delete(member);
+    	
+    	return true;
+//        boolean removed = repository.remove(id);
+//        if(removed){
+//            return true;
+//        }
+//        else {
+//            throw new MemberNotFoundException(id);
+//        }
     }
 
     
