88488ba65e5ae8b5b3a5be58840c811a4b8e0963
Jayhmz <Jayhmz10@gmail.com>
2022-12-02 08:29:09 +0100

    fix(BE-94) fix merge conflict

--- Apply-For-Me-Api/src/main/java/com/hydraulic/applyforme/config/aws/AwsConfig.java
+++ Apply-For-Me-Api/src/main/java/com/hydraulic/applyforme/config/aws/AwsConfig.java
@@ -11,7 +11,7 @@ import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 
 @Configuration
-public class AwsConfig {
+public class AWSConfig {
 
     @Value("${aws.access-key}")
     private String accessKeyId;
