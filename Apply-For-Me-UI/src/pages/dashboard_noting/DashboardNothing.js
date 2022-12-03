0b54ca01a9e5eaa7386e468fa372fde573910a6e
olathegreat <ademolarotimi2000@gmail.com>
2022-12-03 11:12:18 +0100

    feat:newpages added

---
+++ Apply-For-Me-UI/src/pages/dashboard_noting/DashboardNothing.js
@@ -0,0 +1,22 @@
+import React from "react";
+import NewUserDashboard from "./NewUserDashboard";
+// import OldUserDashboard from "./OldUserDashboard";
+
+
+
+
+const DashboardNothing = () => {
+
+
+    return(
+        <div>
+            {/* <OldUserDashboard/> */}
+            <NewUserDashboard/>
+        </div>
+    
+       
+
+    )
+};
+
+export default DashboardNothing;
