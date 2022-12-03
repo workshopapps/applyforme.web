753c198e164a67087e7f513029717696cea0ced6
omoluabidotcom <yyahaya222@yahoo.com>
2022-12-03 07:30:49 +0100

    fix(BAC-111): redundant code remove

--- Apply-For-Me-Api/src/main/java/com/hydraulic/applyforme/repository/SuperAdminCustomRepository.java
+++
@@ -1,4 +0,0 @@
-package com.hydraulic.applyforme.repository;
-
-public interface SuperAdminCustomRepository {
-}
--- Apply-For-Me-Api/src/main/java/com/hydraulic/applyforme/repository/impl/SuperAdminCustomRepositoryImpl.java
+++
@@ -1,8 +0,0 @@
-package com.hydraulic.applyforme.repository.impl;
-
-import com.hydraulic.applyforme.repository.SuperAdminCustomRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public class SuperAdminCustomRepositoryImpl implements SuperAdminCustomRepository {
-}
