f5b6e2cb9ee02a33d7c4223289108ddc076dd204
ginohmk <michotall95@gmail.com>
2022-12-03 21:35:45 +0100

    fix: Refreash issue on protected routes foing to home

--- Apply-For-Me-UI/src/ProtectedRoute.jsx
+++ Apply-For-Me-UI/src/ProtectedRoute.jsx
@@ -12,7 +12,7 @@ const ProtectedRoute = ({ children, allowedRoles }) => {
     ) : user ? (
         <Navigate to="*" state={{ from: location }} replace />
     ) : (
-        <Navigate to="/" state={{ from: location }} replace />
+        <Outlet />
     );
 };
 
