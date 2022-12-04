1051302576d5d976c255b14728d79a95f78ed8d8
#Lay <82811450+AleeyahAnjola@users.noreply.github.com>
2022-12-04 16:15:44 +0100

    Made changes to my controller implementation

--- Apply-For-Me-Api/src/main/java/com/hydraulic/applyforme/controller/ApplicantController.java
+++ Apply-For-Me-Api/src/main/java/com/hydraulic/applyforme/controller/ApplicantController.java
@@ -2,6 +2,7 @@ package com.hydraulic.applyforme.controller;
 
 import com.hydraulic.applyforme.model.domain.Member;
 import com.hydraulic.applyforme.service.impl.ApplicantServiceImpl;
+import com.hydraulic.applyforme.util.CurrentUserUtil;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,9 +20,11 @@ public class ApplicantController {
         this.applicantService = applicantService;
     }
 
-    @GetMapping("/details/{id}")
-    public Member getMyDetails(@PathVariable(value = "id") Long id){
-        return applicantService.getDetails(id);
+    @GetMapping("/details")
+    public Member getMyDetails(Long id){
+        var authenticatedUser = CurrentUserUtil.getCurrentUser();
+        assert authenticatedUser != null;
+        return applicantService.getDetails(authenticatedUser.getId());
     }
 }
 
