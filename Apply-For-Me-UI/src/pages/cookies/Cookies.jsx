00f6dcff3fc03d0e552af4cfd11a19d76537f7d3
volunux <volunux@gmail.com>
2022-12-05 14:59:37 +0100

    remove ui folder

--- Apply-For-Me-UI/src/pages/cookies/Cookies.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Cookies = () => {
-    return <div>Cookies Page</div>;
-};
-
-export default Cookies;
--- Apply-For-Me-UI/src/pages/landing_page/LandingPage.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const LandingPage = () => {
-    return <div>Landing Page</div>;
-};
-
-export default LandingPage;
