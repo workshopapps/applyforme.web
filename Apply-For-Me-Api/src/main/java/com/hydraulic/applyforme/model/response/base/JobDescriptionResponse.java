95cc59b61f49c8734a5c273453ccb0e34ace8e60
Valentine Ekechukwu <val.ekechukwu@gmail.com>
2022-12-06 14:06:30 +0100

    Create JobDescriptionResponse.java

---
+++ Apply-For-Me-Api/src/main/java/com/hydraulic/applyforme/model/response/base/JobDescriptionResponse.java
@@ -0,0 +1,20 @@
+package com.hydraulic.applyforme.model.response.base;
+
+
+public class JobDescriptionResponse {
+
+    public  String jobTitle;
+
+    public String jobCompany;
+
+    public String jobLocation;
+
+    public String date;
+
+    public String monthlySalaryRange;
+
+    public String jobSummary;
+
+    public String Responsibilities;
+
+}
