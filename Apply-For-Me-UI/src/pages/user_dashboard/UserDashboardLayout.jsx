a25f8f4b4c45ba92ce2cf2b6eb8d53f5188d4fc2
hamzat <lawalhamzat27@gmail.com>
2022-12-07 15:32:07 +0100

    remove useSelector

--- Apply-For-Me-UI/src/pages/user_dashboard/UserDashboardLayout.jsx
+++ Apply-For-Me-UI/src/pages/user_dashboard/UserDashboardLayout.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import DashboardSidebar from "../../components/dashboard_sidebar/DashboardSidebar";
 import { Outlet } from "react-router-dom";
 import classes from "./UserDashboardLayout.module.css";
-import { useSelector } from "react-redux";
 const UserDashboardLayout = () => {
 
     return (
