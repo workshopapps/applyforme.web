53d2dcd1e79d101900713ef959c2f0ff86b7e265
Segun <lawalsegun72@gmail.com>
2022-12-09 17:53:12 +0100

    feat: Add old user profile

--- Apply-For-Me-UI/src/pages/dashboard_profile/Profile/Profile.jsx
+++ Apply-For-Me-UI/src/pages/dashboard_profile/Profile/Profile.jsx
@@ -1,39 +1,28 @@
 import TopBar from "../components/TopBar/TopBar";
 import styles from "./Profile.module.css";
-import { Link, NavLink } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import add from "../assets/add.png";
 
 // eslint-disable-next-line no-unused-vars
-const Profile = () => {
-    const ProfileDetails = [
-        {
-            jobtitle: "Product Designer",
-            id: 1,
-            jobtype: "Hybrid",
-            location: "London"
-        },
-        {
-            jobtitle: "Game Developer",
-            id: 2,
-            jobtype: "Remote",
-            location: "Nigeria"
-        },
-        {
-            jobtitle: "Musician",
-            id: 3,
-            jobtype: "Remote",
-            location: "Ghana"
-        }
-    ];
-    const CurrentProfile = ProfileDetails.map(oneprofile => (
+const Profile = ({ profileList }) => {
+    const navigate = useNavigate();
+
+    const CurrentProfile = profileList?.map(oneprofile => (
         <div key={oneprofile.id} className={styles.each_profile}>
             <div className={styles.edit_box}>
-                <h3>{oneprofile.jobtitle}</h3>
+                <h3>{oneprofile.profileTitle}</h3>
                 <div className={styles.job_deets}>
-                    <p>{oneprofile.jobtype}</p>
-                    <p>{oneprofile.location}</p>
+                    <p>{oneprofile.preferredJobLocationType}</p>
+                    <p>{oneprofile.jobLocation}</p>
                 </div>
-                <Link to={`/dashboard/user/${oneprofile.id}`}>See details</Link>
+                <Link
+                    to={{
+                        pathname: `/dashboard/user/${oneprofile.id}`,
+                        state: { oneprofile }
+                    }}
+                >
+                    See details
+                </Link>
             </div>
         </div>
     ));
@@ -41,15 +30,13 @@ const Profile = () => {
     return (
         <div className={styles.profile_page}>
             <TopBar />
-            <NavLink
-                to="/dashboard/user/create-profile"
-                style={{ textDecoration: "none" }}
+            <button
+                className={styles.createjob_button}
+                onClick={() => navigate("/dashboard/user/create-profile")}
             >
-                <button className={styles.createjob_button}>
-                    <p>Create a job profile</p>
-                    <img src={add} alt="add" />
-                </button>
-            </NavLink>
+                <p>Create a job profile</p>
+                <img src={add} alt="add" />
+            </button>
             <div>{CurrentProfile}</div>
         </div>
     );
