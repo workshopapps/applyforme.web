8005d83624b1369a3a374a9e6105c724a603eade
Valentine Ekechukwu <val.ekechukwu@gmail.com>
2022-12-10 12:56:20 +0100

    Created the getJobDescription Controller.

--- Apply-For-Me-Api/src/main/java/com/hydraulic/applyforme/controller/ProfessionalController.java
+++ Apply-For-Me-Api/src/main/java/com/hydraulic/applyforme/controller/ProfessionalController.java
@@ -7,6 +7,7 @@ import static com.hydraulic.applyforme.constants.PagingConstants.DEFAULT_SORT_DI
 
 import java.util.List;
 
+import com.hydraulic.applyforme.model.response.JobDescriptionResponse;
 import org.springframework.data.domain.Page;
 import org.springframework.http.MediaType;
 import org.springframework.validation.annotation.Validated;
@@ -67,5 +68,12 @@ public class ProfessionalController {
 		UserDetailsImpl currentUser = CurrentUserUtil.getCurrentUser();
 		return service.findAllJobProfile(currentUser.getId());
 	}
+
+    @GetMapping("/view-job-description/{jobId}")
+    public JobDescriptionResponse getJobDescription(@PathVariable Long jobId){
+        var currentUser = CurrentUserUtil.getCurrentUser();
+        return service.viewJobDescription(currentUser.getId(), jobId);
+
+    }
 }
 
