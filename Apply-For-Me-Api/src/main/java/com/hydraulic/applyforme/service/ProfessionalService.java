c97223ff1712959cea63fbc4bfe0f979c1a006e0
Emmanuel Oladipupo <emmanuel.oladipupo@decagon.dev>
2022-12-11 19:08:05 +0100

    feat(BE-view-application-summary):replaced method signature from emailAddress to id

--- Apply-For-Me-Api/src/main/java/com/hydraulic/applyforme/service/ProfessionalService.java
+++ Apply-For-Me-Api/src/main/java/com/hydraulic/applyforme/service/ProfessionalService.java
@@ -2,6 +2,7 @@ package com.hydraulic.applyforme.service;
 
 import java.util.List;
 
+import com.hydraulic.applyforme.model.response.JobSummaryResponse;
 import org.springframework.data.domain.Page;
 
 import com.hydraulic.applyforme.model.domain.Professional;
@@ -19,5 +20,7 @@ public interface ProfessionalService {
     Page<Professional> retrieveAllProfessionals(int pageNo, int PageSize);
 
 	List<ProfessionalProfile> findAllJobProfile(Long id);
+    List<JobSummaryResponse> retrieveProfessionalSubmissions(Long id);
+
 
 }
