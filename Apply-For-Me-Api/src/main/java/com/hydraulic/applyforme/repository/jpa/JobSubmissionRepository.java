55fc12663c61f4fbc1976fe49bc77eb5b13bbb20
Emmanuel Oladipupo <emmanuel.oladipupo@decagon.dev>
2022-12-11 10:09:34 +0100

    feat(BE-view-application-summary): added findSubmissionsByProfessional_Id method for retrieving job submissions

--- Apply-For-Me-Api/src/main/java/com/hydraulic/applyforme/repository/jpa/JobSubmissionRepository.java
+++ Apply-For-Me-Api/src/main/java/com/hydraulic/applyforme/repository/jpa/JobSubmissionRepository.java
@@ -6,11 +6,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
+import java.util.Set;
 
 @Repository
 public interface JobSubmissionRepository extends JpaRepository<Submission,Long> {
@@ -23,4 +22,5 @@ public interface JobSubmissionRepository extends JpaRepository<Submission,Long> 
 
      @Query(value = "SELECT applier_id AS ApplierId, COUNT(applier_id) AS ValueOccurrence from job_submission GROUP BY ApplierId ORDER BY COUNT(ApplierId) DESC ", nativeQuery = true)
      List<ApplierResponse> getHighestApplier();
+     Set<Submission> findSubmissionsByProfessional_Id(Long id);
 }
