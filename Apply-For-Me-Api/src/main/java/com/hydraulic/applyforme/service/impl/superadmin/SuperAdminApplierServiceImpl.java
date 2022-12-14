cef442b9a8bfd39464f79bf669b2ac85cbc52844
James Damilare <jayhmz10@gmail.com>
2022-12-14 23:27:13 +0100

    Revert "merge super admin recruiter registration mail to deployment"

--- Apply-For-Me-Api/src/main/java/com/hydraulic/applyforme/service/impl/superadmin/SuperAdminApplierServiceImpl.java
+++ Apply-For-Me-Api/src/main/java/com/hydraulic/applyforme/service/impl/superadmin/SuperAdminApplierServiceImpl.java
@@ -18,7 +18,6 @@ import com.hydraulic.applyforme.repository.*;
 import com.hydraulic.applyforme.repository.jpa.JobSubmissionRepository;
 import com.hydraulic.applyforme.repository.jpa.MemberJpaRepository;
 import com.hydraulic.applyforme.repository.jpa.RoleJpaRepository;
-import com.hydraulic.applyforme.service.EmailService;
 import com.hydraulic.applyforme.service.superadmin.SuperAdminApplierService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -56,9 +55,6 @@ public class SuperAdminApplierServiceImpl implements SuperAdminApplierService {
 
     @Autowired
     private PasswordEncoder passwordEncoder;
-    
-    @Autowired
-    private EmailService emailService;
 
     public SuperAdminApplierServiceImpl(SuperAdminApplierJpaRepository jpaRepository,
                                         SuperAdminApplierRepository superAdminApplierRepository, RoleJpaRepository roleJpaRepository,
@@ -120,7 +116,7 @@ public class SuperAdminApplierServiceImpl implements SuperAdminApplierService {
         }
 
         Member member = mapper.map(dto, Member.class);
-        member.setPassword(passwordEncoder.encode(dto.getPassword()));
+        member.setPassword(dto.getPassword());
         member.getRoles().add(existingRole.get());
         member.setNationality(nationality);
         member.setCountryOfResidence(countryOfResidence);
@@ -131,7 +127,6 @@ public class SuperAdminApplierServiceImpl implements SuperAdminApplierService {
                 .submissions(null)
                 .build();
         applierRepository.saveOne(applier);
-        emailService.sendRegistrationMessageToRecruiter(applier.getMember().getEmailAddress(), dto.getPassword());
         return member;
     }
 
