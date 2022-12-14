1b2a41ac1c99e4240b4b9f263fd9f0386172bda2
Jayhmz <Jayhmz10@gmail.com>
2022-12-14 22:54:58 +0100

    revision: super admin create recruiter and send welcome email containing login details

--- Apply-For-Me-Api/src/main/java/com/hydraulic/applyforme/service/impl/JobSubmissionServiceImpl.java
+++ Apply-For-Me-Api/src/main/java/com/hydraulic/applyforme/service/impl/JobSubmissionServiceImpl.java
@@ -14,6 +14,7 @@ import com.hydraulic.applyforme.repository.SubmissionRepository;
 import com.hydraulic.applyforme.repository.jpa.*;
 import com.hydraulic.applyforme.service.JobSubmissionService;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -26,205 +27,198 @@ import static com.hydraulic.applyforme.util.ApplyForMeUtil.createPageable;
 @Service
 public class JobSubmissionServiceImpl implements JobSubmissionService {
 
-    private final ApplierRepository applierRepository;
-    private final JobSubmissionRepository repository;
-    private final com.hydraulic.applyforme.repository.JobSubmissionRepository repo;
-    private final ModelMapper modelMapper;
-    private final JobSubmissionJpaRepository jpaRepository;
-    private final MemberJpaRepository memberJpaRepository;
-    private final MemberRepository memberRepository;
-    private final ApplierJpaRepository applierJpaRepository;
-    private final SubmissionRepository submissionRepository;
+	private final ApplierRepository applierRepository;
+	private final JobSubmissionRepository repository;
+	private final com.hydraulic.applyforme.repository.JobSubmissionRepository repo;
+	private final ModelMapper modelMapper;
+	private final JobSubmissionJpaRepository jpaRepository;
+	private final MemberJpaRepository memberJpaRepository;
+	private final MemberRepository memberRepository;
+	private final ApplierJpaRepository applierJpaRepository;
+	private final SubmissionRepository submissionRepository;
 
-    private final ProfessionalProfileRepository professionalProfileRepository;
+	@Autowired
+	private ProfessionalJpaRepository professionalJpaRepository;
 
-    private final com.hydraulic.applyforme.repository.JobSubmissionRepository jobSubmissionRepository;
+	private final ProfessionalProfileRepository professionalProfileRepository;
 
-    public JobSubmissionServiceImpl(JobSubmissionRepository repository, ApplierRepository applierRepository, 
-    		com.hydraulic.applyforme.repository.JobSubmissionRepository repo, ModelMapper modelMapper,
-                                    JobSubmissionJpaRepository jpaRepository,
-                                    com.hydraulic.applyforme.repository.JobSubmissionRepository jobSubmissionRepository,
-                                    MemberJpaRepository memberJpaRepository,
-                                    MemberRepository memberRepository,
-                                    ProfessionalJpaRepository professionalJpaRepository,
-                                    ApplierJpaRepository applierJpaRepository,
-                                    ProfessionalProfileRepository professionalProfileRepository,
-                                    SubmissionRepository submissionRepository) {
-        this.applierRepository = applierRepository;
-        this.repository = repository;
-        this.repo = repo;
-        this.modelMapper = modelMapper;
-        this.jpaRepository = jpaRepository;
-        this.jobSubmissionRepository = jobSubmissionRepository;
-        this.memberJpaRepository = memberJpaRepository;
-        this.memberRepository = memberRepository;
-        this.applierJpaRepository = applierJpaRepository;
-        this.professionalProfileRepository = professionalProfileRepository;
-        this.submissionRepository = submissionRepository;
-    }
+	private final com.hydraulic.applyforme.repository.JobSubmissionRepository jobSubmissionRepository;
 
-    @Override
-    @Transactional(readOnly = true)
-    public Long countAllApplierSubmissions(Long id) {
-        Applier applier = applierRepository.getOne(id);
+	public JobSubmissionServiceImpl(JobSubmissionRepository repository, ApplierRepository applierRepository,
+			com.hydraulic.applyforme.repository.JobSubmissionRepository repo, ModelMapper modelMapper,
+			JobSubmissionJpaRepository jpaRepository,
+			com.hydraulic.applyforme.repository.JobSubmissionRepository jobSubmissionRepository,
+			MemberJpaRepository memberJpaRepository, MemberRepository memberRepository,
+			ProfessionalJpaRepository professionalJpaRepository, ApplierJpaRepository applierJpaRepository,
+			ProfessionalProfileRepository professionalProfileRepository, SubmissionRepository submissionRepository) {
+		this.applierRepository = applierRepository;
+		this.repository = repository;
+		this.repo = repo;
+		this.modelMapper = modelMapper;
+		this.jpaRepository = jpaRepository;
+		this.jobSubmissionRepository = jobSubmissionRepository;
+		this.memberJpaRepository = memberJpaRepository;
+		this.memberRepository = memberRepository;
+		this.applierJpaRepository = applierJpaRepository;
+		this.professionalProfileRepository = professionalProfileRepository;
+		this.submissionRepository = submissionRepository;
+	}
 
-        if (applier == null) {
-            throw new ApplierNotFoundException(id);
-        }
-        return repository.countByApplier(id);
-    }
+	@Override
+	@Transactional(readOnly = true)
+	public Long countAllApplierSubmissions(Long id) {
+		Applier applier = applierRepository.getOne(id);
 
+		if (applier == null) {
+			throw new ApplierNotFoundException(id);
+		}
+		return repository.countByApplier(id);
+	}
 
-    @Override
-    @Transactional(readOnly = true)
-    public ApplyForMeResponse getAllJobSubmission(int pageNo, int pageSize, String sortBy, String sortDir) {
-        Page<Submission> submission = repository.findAll(createPageable(pageNo, pageSize, sortBy, sortDir));
-        return getSubmissionResponse(submission);
-    }
+	@Override
+	@Transactional(readOnly = true)
+	public ApplyForMeResponse getAllJobSubmission(int pageNo, int pageSize, String sortBy, String sortDir) {
+		Page<Submission> submission = repository.findAll(createPageable(pageNo, pageSize, sortBy, sortDir));
+		return getSubmissionResponse(submission);
+	}
 
-    @Override
-    public ApplyForMeResponse filterJobSubmission(int pageNo, int pageSize, String sortBy, String sortDir, String q) {
-        Page<Submission> submission = repository.findJobSubmissionBySearch(createPageable(pageNo, pageSize, sortBy, sortDir), q);
-        return getSubmissionResponse(submission);
-    }
+	@Override
+	public ApplyForMeResponse filterJobSubmission(int pageNo, int pageSize, String sortBy, String sortDir, String q) {
+		Page<Submission> submission = repository
+				.findJobSubmissionBySearch(createPageable(pageNo, pageSize, sortBy, sortDir), q);
+		return getSubmissionResponse(submission);
+	}
 
-    private ApplyForMeResponse getSubmissionResponse(Page<Submission> submission) {
-        Collection<SubmissionDto> submissions = submission
-                .getContent()
-                .stream()
-                .map(x -> {
-                    return modelMapper.map(x, SubmissionDto.class);
-                })
-                .collect(Collectors.toList());
+	private ApplyForMeResponse getSubmissionResponse(Page<Submission> submission) {
+		Collection<SubmissionDto> submissions = submission.getContent().stream().map(x -> {
+			return modelMapper.map(x, SubmissionDto.class);
+		}).collect(Collectors.toList());
 
-        SubmissionEntriesResponse response = new SubmissionEntriesResponse();
-        response.setPageNo(submission.getNumber());
-        response.setContent(submissions);
-        response.setPageSize(submission.getSize());
-        response.setTotalElements(submission.getTotalElements());
-        response.setTotalPages(submission.getTotalPages());
-        response.setLast(submission.isLast());
-        return response;
-    }
+		SubmissionEntriesResponse response = new SubmissionEntriesResponse();
+		response.setPageNo(submission.getNumber());
+		response.setContent(submissions);
+		response.setPageSize(submission.getSize());
+		response.setTotalElements(submission.getTotalElements());
+		response.setTotalPages(submission.getTotalPages());
+		response.setLast(submission.isLast());
+		return response;
+	}
 
-    public List<ApplierSubmissionDto> getApplierSubmissionDetails(Long id) {
-        List<ApplierSubmissionDto> applierSubmissionDtos = repo.getSubmissionDetails(id);
-        return applierSubmissionDtos;
-    }
+	public List<ApplierSubmissionDto> getApplierSubmissionDetails(Long id) {
+		List<ApplierSubmissionDto> applierSubmissionDtos = repo.getSubmissionDetails(id);
+		return applierSubmissionDtos;
+	}
 
-    @Override
-    @Transactional(readOnly = true)
-    public ApplyForMeResponse getEntries(int pageNo, int pageSize, String sortBy, String sortDir, String q, Date from, Date to) {
-        Page<Submission> submissions = null;
+	@Override
+	@Transactional(readOnly = true)
+	public ApplyForMeResponse getEntries(int pageNo, int pageSize, String sortBy, String sortDir, String q, Date from,
+			Date to) {
+		Page<Submission> submissions = null;
 
-        if (from != null && to != null && q != null && q.trim() != "") {
-            submissions = jpaRepository.getEntries(from, to, q, createPageable(pageNo, pageSize, sortBy, sortDir));
-        }
-        else if (from != null && to != null) {
-            submissions = jpaRepository.getEntries(from, to, createPageable(pageNo, pageSize, sortBy, sortDir));
-        }
-        if (q != null && q.trim() != "") {
-            submissions = jpaRepository.getEntries(q, createPageable(pageNo, pageSize, sortBy, sortDir));
-        }
-        else {
-            submissions = jpaRepository.getEntries(createPageable(pageNo, pageSize, sortBy, sortDir));
-        }
-        return getJobSubmissionResponse(submissions);
-    }
+		if (from != null && to != null && q != null && q.trim() != "") {
+			submissions = jpaRepository.getEntries(from, to, q, createPageable(pageNo, pageSize, sortBy, sortDir));
+		} else if (from != null && to != null) {
+			submissions = jpaRepository.getEntries(from, to, createPageable(pageNo, pageSize, sortBy, sortDir));
+		}
+		if (q != null && q.trim() != "") {
+			submissions = jpaRepository.getEntries(q, createPageable(pageNo, pageSize, sortBy, sortDir));
+		} else {
+			submissions = jpaRepository.getEntries(createPageable(pageNo, pageSize, sortBy, sortDir));
+		}
+		return getJobSubmissionResponse(submissions);
+	}
 
-    @Override
-    @Transactional(readOnly = true)
-    public ApplyForMeResponse getUserEntries(int pageNo, int pageSize, String sortBy, String sortDir, String q, Date from, Date to, Long userId) {
-        Page<Submission> submissions = null;
+	@Override
+	@Transactional(readOnly = true)
+	public ApplyForMeResponse getUserEntries(int pageNo, int pageSize, String sortBy, String sortDir, String q,
+			Date from, Date to, Long userId) {
+		Page<Submission> submissions = null;
 
-        if (from != null && to != null && q != null && q.trim() != "") {
-            submissions = jpaRepository.getUserEntries(from, to, q, userId, createPageable(pageNo, pageSize, sortBy, sortDir));
-        }
-        else if (from != null && to != null) {
-            submissions = jpaRepository.getUserEntries(from, to, userId, createPageable(pageNo, pageSize, sortBy, sortDir));
-        }
-        if (q != null && q.trim() != "") {
-            submissions = jpaRepository.getUserEntries(q, userId, createPageable(pageNo, pageSize, sortBy, sortDir));
-        }
-        else {
-            submissions = jpaRepository.getUserEntries(userId, createPageable(pageNo, pageSize, sortBy, sortDir));
-        }
-        return getJobSubmissionResponse(submissions);
-    }
+		if (from != null && to != null && q != null && q.trim() != "") {
+			submissions = jpaRepository.getUserEntries(from, to, q, userId,
+					createPageable(pageNo, pageSize, sortBy, sortDir));
+		} else if (from != null && to != null) {
+			submissions = jpaRepository.getUserEntries(from, to, userId,
+					createPageable(pageNo, pageSize, sortBy, sortDir));
+		}
+		if (q != null && q.trim() != "") {
+			submissions = jpaRepository.getUserEntries(q, userId, createPageable(pageNo, pageSize, sortBy, sortDir));
+		} else {
+			submissions = jpaRepository.getUserEntries(userId, createPageable(pageNo, pageSize, sortBy, sortDir));
+		}
+		return getJobSubmissionResponse(submissions);
+	}
 
-    @Override
-    public Submission findOne(Long id) {
-        Submission submission = jobSubmissionRepository.getOne(id);
-        submission.getProfessional().setSubmissions(null);
-        submission.getApplier().setSubmissions(null);
-        submission.getProfessional().getMember().setRoles(null);
-        submission.getProfessional().setProfessionalProfiles(null);
-        return submission;
-    }
+	@Override
+	public Submission findOne(Long id) {
+		Submission submission = jobSubmissionRepository.getOne(id);
+		submission.getProfessional().setSubmissions(null);
+		submission.getApplier().setSubmissions(null);
+		submission.getProfessional().getMember().setRoles(null);
+		submission.getProfessional().setProfessionalProfiles(null);
+		return submission;
+	}
 
-    @Override
-    public Submission findOne(Long memberId, Long id) {
-        Submission submission = jpaRepository.getOneUserSubmission(memberId, id);
-        submission.getProfessional().setSubmissions(null);
-        submission.getApplier().setSubmissions(null);
-        submission.getProfessional().getMember().setRoles(null);
-        submission.getProfessional().setProfessionalProfiles(null);
-        return submission;
-    }
+	@Override
+	public Submission findOne(Long memberId, Long id) {
+		Submission submission = jpaRepository.getOneUserSubmission(memberId, id);
+		submission.getProfessional().setSubmissions(null);
+		submission.getApplier().setSubmissions(null);
+		submission.getProfessional().getMember().setRoles(null);
+		submission.getProfessional().setProfessionalProfiles(null);
+		return submission;
+	}
 
-    private ApplyForMeResponse getJobSubmissionResponse(Page<Submission> submissions) {
-        Collection<Submission> results = submissions
-                .getContent()
-                .stream()
-                .map(x -> {
-                    x.getProfessional().setSubmissions(null);
-                    x.getProfessional().getMember().setRoles(null);
-                    x.getProfessional().setProfessionalProfiles(null);
-                    x.getApplier().setSubmissions(null);
-                    x.getApplier().getMember().setRoles(null);
-                    return modelMapper.map(x, Submission.class);
-                })
-                .collect(Collectors.toList());
+	private ApplyForMeResponse getJobSubmissionResponse(Page<Submission> submissions) {
+		Collection<Submission> results = submissions.getContent().stream().map(x -> {
+			x.getProfessional().setSubmissions(null);
+			x.getProfessional().getMember().setRoles(null);
+			x.getProfessional().setProfessionalProfiles(null);
+			x.getApplier().setSubmissions(null);
+			x.getApplier().getMember().setRoles(null);
+			return modelMapper.map(x, Submission.class);
+		}).collect(Collectors.toList());
 
-        ApplyForMeResponse response = new ApplyForMeResponse();
-        response.setContent(results);
-        response.setPageNo(submissions.getNumber());
-        response.setPageSize(submissions.getSize());
-        response.setTotalElements(submissions.getTotalElements());
-        response.setTotalPages(submissions.getTotalPages());
-        response.setLast(submissions.isLast());
-        return response;
-    }
+		ApplyForMeResponse response = new ApplyForMeResponse();
+		response.setContent(results);
+		response.setPageNo(submissions.getNumber());
+		response.setPageSize(submissions.getSize());
+		response.setTotalElements(submissions.getTotalElements());
+		response.setTotalPages(submissions.getTotalPages());
+		response.setLast(submissions.isLast());
+		return response;
+	}
 
-    @Override
-    @Transactional
-    public Submission saveSubmission(CreateJobSubmissionDto dto) {
-        Professional existingProfessional = professionalJpaRepository.getProfessional(dto.getProfessionalId());
-        if (existingProfessional == null) {
-            throw new ProfessionalNotFoundException(dto.getProfessionalId());
-        }
+	@Override
+	@Transactional
+	public Submission saveSubmission(CreateJobSubmissionDto dto) {
+		Professional existingProfessional = professionalJpaRepository.getProfessional(dto.getProfessionalId());
+		if (existingProfessional == null) {
+			throw new ProfessionalNotFoundException(dto.getProfessionalId());
+		}
 
-        Applier existingApplier = applierJpaRepository.getApplier(dto.getApplierId());
-        if (existingApplier == null) {
-            throw new ApplierNotFoundException(dto.getApplierId());
-        }
+		Applier existingApplier = applierJpaRepository.getApplier(dto.getApplierId());
+		if (existingApplier == null) {
+			throw new ApplierNotFoundException(dto.getApplierId());
+		}
 
-        ProfessionalProfile existingProfessionalProfile = professionalProfileRepository.getOne(dto.getProfessionalProfileId());
-        if (existingProfessionalProfile == null) {
-            throw new ProfessionalProfileNotFoundException(dto.getProfessionalProfileId());
-        }
+		ProfessionalProfile existingProfessionalProfile = professionalProfileRepository
+				.getOne(dto.getProfessionalProfileId());
+		if (existingProfessionalProfile == null) {
+			throw new ProfessionalProfileNotFoundException(dto.getProfessionalProfileId());
+		}
 
-        Submission submission = modelMapper.map(dto, Submission.class);
-        submission.setProfessional(existingProfessional);
-        submission.setProfessionalProfile(existingProfessionalProfile);
-        submission.setApplier(existingApplier);
-        submissionRepository.saveOne(submission);
+		Submission submission = modelMapper.map(dto, Submission.class);
+		submission.setProfessional(existingProfessional);
+		submission.setProfessionalProfile(existingProfessionalProfile);
+		submission.setApplier(existingApplier);
+		submissionRepository.saveOne(submission);
 
-        submission.getProfessional().setSubmissions(null);
-        submission.getApplier().setSubmissions(null);
-        submission.getProfessional().getMember().setRoles(null);
-        submission.getProfessional().setProfessionalProfiles(null);
-        return submission;
-    }
+		submission.getProfessional().setSubmissions(null);
+		submission.getApplier().setSubmissions(null);
+		submission.getProfessional().getMember().setRoles(null);
+		submission.getProfessional().setProfessionalProfiles(null);
+		return submission;
+	}
 
 }
