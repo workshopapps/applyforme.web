03204f13e7add844a07e769c04841fac65497d90
omoluabidotcom <yyahaya222@yahoo.com>
2022-12-15 15:39:16 +0100

    feat/ recruiter dashboard statistics

--- Apply-For-Me-Api/src/main/java/com/hydraulic/applyforme/service/RecruiterApplicantService.java
+++ Apply-For-Me-Api/src/main/java/com/hydraulic/applyforme/service/RecruiterApplicantService.java
@@ -2,6 +2,7 @@ package com.hydraulic.applyforme.service;
 
 import com.hydraulic.applyforme.model.domain.Member;
 import com.hydraulic.applyforme.model.response.ApplicantDetailsResponse;
+import com.hydraulic.applyforme.model.response.RecruiterStats;
 import com.hydraulic.applyforme.model.response.base.ApplyForMeResponse;
 
 import java.util.Date;
@@ -9,6 +10,6 @@ import java.util.Date;
 public interface RecruiterApplicantService {
     ApplyForMeResponse getEntries(int pageNo, int pageSize, String sortBy, String sortDir, String q, Date from, Date to);
     ApplicantDetailsResponse getOne(Long id);
-
     Member getOneMember(Long id);
+    RecruiterStats getRecruiterStats();
 }
