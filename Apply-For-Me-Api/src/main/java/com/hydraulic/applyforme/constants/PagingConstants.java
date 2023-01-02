614ae2ad7888909d743913f140ae2042b68a73ac
omoluabidotcom <yyahaya222@yahoo.com>
2023-01-02 14:59:15 +0100

    feat/three endpoints to process payment with paystack

--- Apply-For-Me-Api/src/main/java/com/hydraulic/applyforme/constants/PagingConstants.java
+++ Apply-For-Me-Api/src/main/java/com/hydraulic/applyforme/constants/PagingConstants.java
@@ -1,9 +1,18 @@
 package com.hydraulic.applyforme.constants;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public class PagingConstants {
     public static final String DEFAULT_PAGE_NUMBER = "0";
 
     public static final String DEFAULT_PAGE_SIZE = "10";
     public static final String DEFAULT_SORT_BY = "id";
     public static final String DEFAULT_SORT_DIRECTION = "asc";
+    public static final Integer STATUS_CODE_OK = 200;
+    public static final Integer STATUS_CODE_CREATED = 201;
+    public static final String PAYSTACK_INIT = "https://api.paystack.co/plan";
+    @Value(value = "")
+    public static final String PAYSTACK_SECRET_KEY = "sk_test_be5761495c667f7dec5823032c1548f552e9e0e9";
+    public static final String PAYSTACK_INITIALIZE_PAY = "https://api.paystack.co/transaction/initialize";
+    public static final String PAYSTACK_VERIFY = "https://api.paystack.co/transaction/verify/";
 }
---
+++ Apply-For-Me-Api/src/main/java/com/hydraulic/applyforme/service/impl/payment/PaystackServiceImpl.java
@@ -0,0 +1,174 @@
+package com.hydraulic.applyforme.service.impl.payment;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+import com.hydraulic.applyforme.model.domain.Member;
+import com.hydraulic.applyforme.model.domain.PaymentPaystack;
+import com.hydraulic.applyforme.model.dto.payment.paystack.CreatePlanDto;
+import com.hydraulic.applyforme.model.dto.payment.paystack.InitializePaymentDto;
+import com.hydraulic.applyforme.model.enums.PricingPlanType;
+import com.hydraulic.applyforme.model.exception.NotAuthorisedException;
+import com.hydraulic.applyforme.model.exception.PaymentInitializationException;
+import com.hydraulic.applyforme.model.exception.PaymentVerificationException;
+import com.hydraulic.applyforme.model.exception.PlanCreationException;
+import com.hydraulic.applyforme.model.response.payment.paystack.CreatePlanResponse;
+import com.hydraulic.applyforme.model.response.payment.paystack.InitializePaymentResponse;
+import com.hydraulic.applyforme.model.response.payment.paystack.PaymentVerificationResponse;
+import com.hydraulic.applyforme.repository.PaystackPaymentRepository;
+import com.hydraulic.applyforme.repository.jpa.MemberJpaRepository;
+import com.hydraulic.applyforme.service.payment.PaystackService;
+import com.hydraulic.applyforme.util.CurrentUserUtil;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Date;
+
+import static com.hydraulic.applyforme.constants.PagingConstants.*;
+
+@Service
+public class PaystackServiceImpl implements PaystackService {
+
+    private final PaystackPaymentRepository paystackPaymentRepository;
+    private final MemberJpaRepository memberJpaRepository;
+
+    public PaystackServiceImpl(PaystackPaymentRepository paystackPaymentRepository, MemberJpaRepository memberJpaRepository) {
+        this.paystackPaymentRepository = paystackPaymentRepository;
+        this.memberJpaRepository = memberJpaRepository;
+    }
+
+    @Override
+    public CreatePlanResponse createPlan(CreatePlanDto createPlanDto) throws Exception {
+        CreatePlanResponse createPlanResponse = null;
+
+        try {
+            Gson gson = new Gson();
+            StringEntity postingString = new StringEntity(gson.toJson(createPlanDto));
+            HttpClient client = HttpClientBuilder.create().build();
+            HttpPost post = new HttpPost(PAYSTACK_INIT);
+            post.setEntity(postingString);
+            post.addHeader("Content-type", "application/json");
+            post.addHeader("Authorization", "Bearer " + PAYSTACK_SECRET_KEY);
+            StringBuilder result = new StringBuilder();
+            HttpResponse response = client.execute(post);
+
+            if (response.getStatusLine().getStatusCode() == STATUS_CODE_CREATED) {
+
+                BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+
+                String line;
+                while ((line = rd.readLine()) != null) {
+                    result.append(line);
+                }
+            } else {
+                throw new PlanCreationException("Paystack");
+            }
+
+            ObjectMapper mapper = new ObjectMapper();
+            createPlanResponse = mapper.readValue(result.toString(), CreatePlanResponse.class);
+        } catch(Throwable ex) {
+            throw new NotAuthorisedException("Paystack");
+        }
+        return createPlanResponse;
+    }
+
+    @Override
+    public InitializePaymentResponse initializePayment(InitializePaymentDto initializePaymentDto) {
+        InitializePaymentResponse initializePaymentResponse = null;
+
+        try {
+            Gson gson = new Gson();
+            StringEntity postingString = new StringEntity(gson.toJson(initializePaymentDto));
+            HttpClient client = HttpClientBuilder.create().build();
+            HttpPost post = new HttpPost(PAYSTACK_INITIALIZE_PAY);
+            post.setEntity(postingString);
+            post.addHeader("Content-type", "application/json");
+            post.addHeader("Authorization", "Bearer " + PAYSTACK_SECRET_KEY);
+            StringBuilder result = new StringBuilder();
+            HttpResponse response = client.execute(post);
+
+            if (response.getStatusLine().getStatusCode() == STATUS_CODE_OK) {
+
+                BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+
+                String line;
+                while ((line = rd.readLine()) != null) {
+                    result.append(line);
+                }
+            } else {
+                throw new PaymentInitializationException("Paystack");
+            }
+
+            ObjectMapper mapper = new ObjectMapper();
+            initializePaymentResponse = mapper.readValue(result.toString(), InitializePaymentResponse.class);
+        } catch(Throwable ex) {
+            throw new NotAuthorisedException("Paystack");
+        }
+        return initializePaymentResponse;
+    }
+
+    @Override
+    @Transactional
+    public PaymentVerificationResponse paymentVerification(String reference, String plan) throws Exception {
+        PaymentVerificationResponse paymentVerificationResponse = null;
+        PaymentPaystack paymentPaystack = null;
+
+        try{
+            HttpClient client = HttpClientBuilder.create().build();
+            HttpGet request = new HttpGet(PAYSTACK_VERIFY + reference);
+            request.addHeader("Content-type", "application/json");
+            request.addHeader("Authorization", "Bearer " + PAYSTACK_SECRET_KEY);
+            StringBuilder result = new StringBuilder();
+            HttpResponse response = client.execute(request);
+
+            if (response.getStatusLine().getStatusCode() == STATUS_CODE_OK) {
+                BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+                String line;
+
+                while ((line = rd.readLine()) != null) {
+                    result.append(line);
+                }
+            } else {
+                throw new PaymentVerificationException("Paystack");
+            }
+
+            ObjectMapper mapper = new ObjectMapper();
+            paymentVerificationResponse = mapper.readValue(result.toString(), PaymentVerificationResponse.class);
+
+            if (paymentVerificationResponse == null || paymentVerificationResponse.getStatus().equals("false")) {
+                throw new Exception("An error");
+            } else if (paymentVerificationResponse.getData().getStatus().equals("success")) {
+
+                var currentUser = CurrentUserUtil.getCurrentUser();
+                Member member = memberJpaRepository.findById(currentUser.getId()).get();
+                PricingPlanType pricingPlanType = PricingPlanType.valueOf(plan.toUpperCase());
+//                Member member = memberJpaRepository.findById(Long.valueOf(7801)).get();
+
+                paymentPaystack = PaymentPaystack.builder()
+                        .member(member)
+                        .reference(paymentVerificationResponse.getData().getReference())
+                        .amount(paymentVerificationResponse.getData().getAmount())
+                        .gatewayResponse(paymentVerificationResponse.getData().getGatewayResponse())
+                        .paidAt(paymentVerificationResponse.getData().getPaidAt())
+                        .createdAt(paymentVerificationResponse.getData().getCreatedAt())
+                        .channel(paymentVerificationResponse.getData().getChannel())
+                        .currency(paymentVerificationResponse.getData().getCurrency())
+                        .ipAddress(paymentVerificationResponse.getData().getIpAddress())
+                        .pricingPlanType(pricingPlanType)
+                        .createdOn(new Date())
+                        .build();
+            }
+        } catch (Exception ex) {
+            throw new NotAuthorisedException("Paystack");
+        }
+        paystackPaymentRepository.saveOne(paymentPaystack);
+        return paymentVerificationResponse;
+    }
+}
