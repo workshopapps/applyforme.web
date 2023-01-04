95098f9a5b244ac78bfd2051e58e4b3ccd174c7a
Segun <lawalsegun72@gmail.com>
2023-01-04 13:42:59 +0100

    initial commit

--- Apply-For-Me-UI/src/pages/admin_dashboard/components/application_list/ApplicationList.jsx
+++ Apply-For-Me-UI/src/pages/admin_dashboard/components/application_list/ApplicationList.jsx
@@ -1,31 +1,38 @@
 import classes from "../../../RR_Dashboard/styles/Applications.module.css";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import axios from 'axios';
+import axios from "axios";
 import { useCallback } from "react";
-
+import Spinner from "components/spinner/Spinner";
 
 const ApplicationList = () => {
     const [listArray, setListArray] = useState();
+    const [isLoading, setIsLoading] = useState(true);
+
     const token = localStorage.getItem("tokenHngKey");
-       const getApplicationDetail = useCallback( async () => {
-           try {
-               const response = await axios.get(
-                   "https://api.applyforme.hng.tech/api/v1/professional-profile/entries/all",
-                   {
-                       headers: {
-                           "Authorization": `Bearer ${token}`
-                       }
-                   }
-               );
-               setListArray(response.data.content);
-           } catch (err) {
-               console.log(err.response?.data);
-           }
-       },[token]);
-       useEffect(() => {
-        getApplicationDetail ();
-       }, [ getApplicationDetail]);
+    const getApplicationDetail = useCallback(async () => {
+        try {
+            const response = await axios.get(
+                "https://api.applyforme.hng.tech/api/v1/professional-profile/entries/all",
+                {
+                    headers: {
+                        "Authorization": `Bearer ${token}`
+                    }
+                }
+            );
+            setListArray(response.data.content);
+            setIsLoading(false);
+        } catch (err) {
+            console.log(err.response?.data);
+        }
+    }, [token]);
+    useEffect(() => {
+        getApplicationDetail();
+    }, [getApplicationDetail]);
+
+    if (isLoading) {
+        return <Spinner />;
+    }
     return (
         // Application stats Table
         <div className={classes.new_applications_stats_table}>
@@ -53,40 +60,53 @@ const ApplicationList = () => {
                 </li>
             </ul>
 
-
-            {
-               listArray?.length!== 0 ? (
-                    listArray?.map((entry, i) => {
-                        return (
-                            // Entry Row
-                            <ul className={classes.stats_row} key={i}>
-                                <li
-                                    className={`${classes.stats_item} ${classes.stats_name}`}
+            {listArray?.length !== 0 ? (
+                listArray?.map((entry, i) => {
+                    return (
+                        // Entry Row
+                        <ul className={classes.stats_row} key={i}>
+                            <li
+                                className={`${classes.stats_item} ${classes.stats_name}`}
+                            >
+                                {entry.profileTitle}
+                            </li>
+                            <li
+                                className={`${classes.stats_item} ${classes.desktop_only}`}
+                            >
+                                {entry.salaryRange}
+                            </li>
+                            <li
+                                className={`${classes.stats_item} ${classes.desktop_only}`}
+                            >
+                                {entry.preferredJobLocationType}
+                            </li>
+                            <li
+                                className={`${classes.stats_item} ${classes.stats_details}`}
+                            >
+                                <Link
+                                    to={`/professional-profile/user/details/${entry.id}`}
+                                    className={
+                                        classes.stats_details_view_button
+                                    }
+                                    style={{
+                                        textDecoration: "none",
+                                        color: "darkslategray",
+                                        padding: "0.5rem",
+                                        border: "1px solid darkslategray",
+                                        borderRadius: "5px"
+                                    }}
                                 >
-                                    {entry.profileTitle}
-                                </li>
-                                <li
-                                    className={`${classes.stats_item} ${classes.desktop_only}`}
-                                >
-                                    {entry.salaryRange}
-                                </li>
-                                <li
-                                    className={`${classes.stats_item} ${classes.desktop_only}`}
-                                >
-                                    {entry.preferredJobLocationType}
-                                </li>
-                                <li
-                                    className={`${classes.stats_item} ${classes.stats_details}`}
-                                >
-                                    <Link to={`/professional-profile/user/details/${entry.id}`} className={classes.stats_details_view_button} style={{textDecoration:"none", color:"darkslategray",padding:"0.5rem", border:"1px solid darkslategray", borderRadius:"5px"}}>
-                                       view
-                                    </Link>
-                                </li>
-                            </ul>
-                        );
-                    })
-                ):<h3 style={{textAlign:"center"}}>No applications have being made</h3>
-            }
+                                    view
+                                </Link>
+                            </li>
+                        </ul>
+                    );
+                })
+            ) : (
+                <h3 style={{ textAlign: "center" }}>
+                    No applications have being made
+                </h3>
+            )}
         </div>
     );
 };
